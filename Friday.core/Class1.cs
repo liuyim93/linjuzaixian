3b3cf9e57c92f1729289a1b0a587adc2b6492628
basilwang <ocam@163.com>
2013-01-11 13:20:29 +0800

    init

---
+++ Friday.core/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Friday.core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Friday.mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Friday.mvc.MvcApplication" Language="C#" %>
