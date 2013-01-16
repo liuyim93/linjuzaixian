b8b9d661046d6a4f701a3f749837ea0aa0ca8644
basilwang <ocam@163.com>
2013-01-16 22:09:35 +0800

    add Commodity

--- Friday.core/domain/MerchantCategory.cs
+++ Friday.core/domain/MerchantCategory.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using friday.core.domain;
 
 namespace friday.core
 {
-    public class MerchantCategory
+    public class MerchantCategory:Entity
     {
         //中餐 西餐 清真
         public virtual String MerchantCategoryName
