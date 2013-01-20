e4c530edddb64ded7486b6e28992d74cb6dd3dad
basilwang <ocam@163.com>
2013-01-20 19:42:52 +0800

    add unity configuration

--- Friday.core/components/ContainerFactory.cs
+++ Friday.core/components/ContainerFactory.cs
@@ -9,16 +9,17 @@ using NHibernate.Cache;
 using System.Web;
 using System.Web.Caching;
 using System.IO;
+using UnityConfiguration;
 
 namespace friday.core.components
 {
     public static class ContainerFactory
     {
-        static object _lock = new Object();   
+        static object _lock = new Object();
         private static IUnityContainer container;
         public static IUnityContainer GetContainer()
         {
-            
+
             if (container == null)
             {
                 lock (_lock)
@@ -27,46 +28,47 @@ namespace friday.core.components
                     {
                         IUnityContainer Container = new UnityContainer();
                         Container.RegisterType<ICache, WebCache>();
+                        UnityRegistry x = new UnityRegistry();
+                        x.AddRegistry<FooRegistry>();
+                        //ExeConfigurationFileMap infraFileMap = new ExeConfigurationFileMap();
+                        //if(HttpContext.Current!=null)
+                        //infraFileMap.ExeConfigFilename = HttpContext.Current.Server.MapPath("~/unity.di.infrastructure.config");
+                        //else
+                        //    infraFileMap.ExeConfigFilename = AppDomain.CurrentDomain.BaseDirectory + "unity.di.infrastructure.config";
 
-                        ExeConfigurationFileMap infraFileMap = new ExeConfigurationFileMap();
-                        if(HttpContext.Current!=null)
-                        infraFileMap.ExeConfigFilename = HttpContext.Current.Server.MapPath("~/unity.di.infrastructure.config");
-                        else
-                            infraFileMap.ExeConfigFilename = AppDomain.CurrentDomain.BaseDirectory + "unity.di.infrastructure.config";
+                        //UnityConfigurationSection infraConfig = (UnityConfigurationSection)ConfigurationManager
+                        //    .OpenMappedExeConfiguration(infraFileMap, ConfigurationUserLevel.None)
+                        //    .GetSection("unity");
+                        //if (infraConfig.Containers.Default != null)
+                        //{
+                        //    infraConfig.Containers.Default.Configure(Container);
+                        //}
 
-                        UnityConfigurationSection infraConfig = (UnityConfigurationSection)ConfigurationManager
-                            .OpenMappedExeConfiguration(infraFileMap, ConfigurationUserLevel.None)
-                            .GetSection("unity");
-                        if (infraConfig.Containers.Default != null)
-                        {
-                            infraConfig.Containers.Default.Configure(Container);
-                        }
+                        ////上面是上篇讲的DI依赖注入XML文件的读取，下面是本篇讲的AOP的XML文件读取
+                        //ExeConfigurationFileMap infraAopFileMap = new ExeConfigurationFileMap();
+                        //if (HttpContext.Current != null)
+                        //    infraAopFileMap.ExeConfigFilename = HttpContext.Current.Server.MapPath("~/unity.aop.infrastructure.config");
+                        //else
+                        //    infraAopFileMap.ExeConfigFilename = AppDomain.CurrentDomain.BaseDirectory + "unity.aop.infrastructure.config";
 
-                        //上面是上篇讲的DI依赖注入XML文件的读取，下面是本篇讲的AOP的XML文件读取
-                        ExeConfigurationFileMap infraAopFileMap = new ExeConfigurationFileMap();
-                        if (HttpContext.Current != null)
-                            infraAopFileMap.ExeConfigFilename = HttpContext.Current.Server.MapPath("~/unity.aop.infrastructure.config");
-                        else
-                            infraAopFileMap.ExeConfigFilename = AppDomain.CurrentDomain.BaseDirectory + "unity.aop.infrastructure.config";
 
-                       
-                        UnityConfigurationSection infraAopConfig = (UnityConfigurationSection)ConfigurationManager
-                            .OpenMappedExeConfiguration(infraAopFileMap, ConfigurationUserLevel.None)
-                            .GetSection("unity");
-                        if (infraAopConfig.Containers.Default != null)
-                        {
-                            infraAopConfig.Containers.Default.Configure(Container);
-                        }
+                        //UnityConfigurationSection infraAopConfig = (UnityConfigurationSection)ConfigurationManager
+                        //    .OpenMappedExeConfiguration(infraAopFileMap, ConfigurationUserLevel.None)
+                        //    .GetSection("unity");
+                        //if (infraAopConfig.Containers.Default != null)
+                        //{
+                        //    infraAopConfig.Containers.Default.Configure(Container);
+                        //}
                         container = Container;
                     }
                 }
                 //HttpContext.Current.Application.Add("container", Container);
 
-                
-               
+
+
             }
             return container;
         }
-        
+
     }
 }
---
+++ Friday.core/components/FooRegistry.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityConfiguration;
+
+namespace friday.core.components
+{
+    public class FooRegistry : UnityRegistry
+    {
+        public FooRegistry()
+       {
+         // Scan one or several assemblies and auto register types based on a
+         // convention. You can also include or exclude certain types and/or
+         // namespaces using a filter.
+         Scan(scan =>
+         {
+           scan.AssemblyContaining<FooRegistry>();
+           scan.With<FirstInterfaceConvention>();
+           //scan.With<AddAllConvention>().TypesImplementing<IHaveManyImplementations>();
+           //scan.With<SetAllPropertiesConvention>().OfType<ILogger>();
+           //scan.ExcludeType<FooService>();
+         });
+ 
+         // Manually register a service
+         //Register<IFooService, FooService>().WithName("Foo").AsSingleton();
+ 
+         // Make services a singleton
+         //MakeSingleton<ISingletonService>();
+ 
+         // You can automatically configure the container to call
+         // a method on any service when they are created
+         //AfterBuildingUp<IStartable>().Call((c, s) => s.Start();
+ 
+         // You can automatically configure the container to
+         // decorate services when they are created
+         //AfterBuildingUp<IFooService>().DecorateWith((c, t) => new FooDecorator(t));
+ 
+         // If you want to inject values or objects that are not registered in
+         // the container, you can do so by adding them using this statement.
+         // For instances you want the container to create, just specify the type.
+         //ConfigureCtorArgsFor<ServiceWithCtorArgs>("some string", typeof (IFooService));
+ 
+         // Unity follows the greedy constructor pattern when creating instances.
+         // If you want to use a different constructor, you specify it by listing
+         // the types of the arguments in the constructor you want it to use.
+         //SelectConstructor<ServiceWithCtorArgs>(typeof (IFooService));
+       }
+    }
+}
