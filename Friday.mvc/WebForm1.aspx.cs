0f451d48090ed860602de190ab460e432697f38f
basilwang <ocam@163.com>
2013-01-21 09:35:18 +0800

    test unityhelper

---
+++ Friday.mvc/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using friday.core.repositories;
+using friday.core.domain;
+using friday.core;
+
+namespace Friday.mvc
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IRepository<SystemUser> repo = UnityHelper.UnityToT<Repository<SystemUser>>();
+            SystemUser u = new SystemUser()
+            {
+                Name = "basil",
+                Password = "123456",
+                Tel = "1342343214",
+                Email = "ocam@163.com",
+                Description = "a lot of things"
+
+
+
+            };
+            repo.SaveOrUpdate(u);
+        }
+    }
+}
