f90ea860f6da5e26156572f5b82a0e917b8552d8
basilwang <ocam@163.com>
2013-01-21 21:19:40 +0800

    test controller unity inject

---
+++ Friday.mvc/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using friday.core.services;
+
+namespace Friday.mvc.Controllers
+{
+    public class TestController : Controller
+    {
+        IUserService userService;
+        public TestController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
