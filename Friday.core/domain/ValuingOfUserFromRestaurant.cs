a493e97495756165f59d01536c863fc19aae7ea3
wanghaichuan <haichuan300@163.com>
2013-01-22 15:25:38 +0800

    add ValuingOfUser

---
+++ Friday.core/domain/ValuingOfUserFromRestaurant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.domain;
+
+namespace friday.core
+{
+    public class ValuingOfUserFromRestaurant:Valuing
+    {
+        public Restaurant FromRestaurant
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+        public SystemUser ToSystemUser
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
---
+++ Friday.core/domain/ValuingOfUserFromShop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.domain;
+
+namespace friday.core
+{
+    public class ValuingOfUserFromShop:Valuing
+    {
+        public Shop FromShop
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+        public SystemUser ToSystemUser
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+    }
+}
