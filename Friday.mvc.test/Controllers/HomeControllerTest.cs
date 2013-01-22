46012a011dc39327039c65b04c1ed2178d90d141
wanghaichuan <haichuan300@163.com>
2013-01-22 13:42:21 +0800

    add mvc test project

---
+++ Friday.mvc.test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Gallio.Framework;
+using MbUnit.Framework;
+using Friday.mvc.Controllers;
+
+
+namespace Friday.mvc.test.Controllers
+{
+    /// <summary>
+    /// Summary description for HomeControllerTest
+    /// </summary>
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Setup
+            HomeController controller = new HomeController();
+
+            // Execute
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Verify
+            ViewDataDictionary viewData = result.ViewData as ViewDataDictionary;
+
+            Assert.AreEqual("Home Page", viewData["Title"]);
+            Assert.AreEqual("Welcome to ASP.NET MVC!", viewData["Message"]);
+        }
+
+    }
+}
---
+++ Friday.mvc.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Friday.mvc.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Friday.mvc.test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fe8e1271-1eaf-44ef-b8a9-1e5df4f6981f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
