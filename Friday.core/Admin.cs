fe57670ca0258ab4453c3cf5d5ecc941998f3e70
wanghaichuan <haichuan300@163.com>
2013-01-24 15:14:47 +0800

    add admin

---
+++ Friday.core/Admin.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.domain;
+
+namespace friday.core
+{
+    public class Admin:Entity
+    {
+        /// <remarks>真实姓名</remarks>
+        public virtual string Name
+        {
+            get;
+
+            set;
+
+        }
+
+        public virtual string LoginName
+        {
+            get;
+
+            set;
+
+        }
+
+        public virtual string Password
+        {
+            get;
+
+            set;
+
+        }
+
+        //2013-01-24 basilwang administrator shopadmin restaurantadmin rentadmin 
+        public virtual int AdminType
+        {
+            get;
+
+            set;
+
+        }
+        public virtual int Merchant
+        {
+            get;
+            set;
+        }
+
+        public virtual string Tel
+        {
+            get;
+
+            set;
+
+        }
+
+        public virtual string Email
+        {
+            get;
+
+            set;
+
+        }
+
+        public virtual string Description
+        {
+            get;
+
+            set;
+
+        }
+    }
+}
