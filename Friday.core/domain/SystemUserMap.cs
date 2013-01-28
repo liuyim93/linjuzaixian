b1dc944b31079051310a1f030d4fe067d08dae71
basilwang <ocam@163.com>
2013-01-28 22:15:13 +0800

    add property-ref for systemuser

--- Friday.core/domain/SystemUserMap.cs
+++ Friday.core/domain/SystemUserMap.cs
@@ -21,7 +21,9 @@ namespace friday.core.domain
             Map(o => o.Name);
             //Map(o => o.Password);
             Map(o => o.Tel);
-            Map(o => o.UserType);;
+            Map(o => o.UserType);
+
+            HasOne<LoginUser>(o => o.LoginUser).PropertyRef("SystemUser");
 
 
         }
