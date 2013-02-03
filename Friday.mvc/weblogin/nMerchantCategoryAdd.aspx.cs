c19e6b8d3a320561b04172b2d0d8dd1fa4be8d37
wanghaichuan <haichuan300@163.com>
2013-02-03 11:26:00 +0800

    Add  nMerchantCategoryAdd

---
+++ Friday.mvc/weblogin/nMerchantCategoryAdd.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using friday.core.repositories;
+using friday.core;
+using friday.core.components;
+using friday.core.domain;
+
+namespace Friday.mvc.weblogin
+{
+    public partial class nMerchantCategoryAdd : System.Web.UI.Page
+    {
+        IRepository<MerchantCategory> iMerchantCategoryRepository = UnityHelper.UnityToT<IRepository<MerchantCategory>>();
+        private MerchantCategory merchantCategory;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Params["__EVENTVALIDATION"] != null)
+            {
+
+                SaveMerchantCategory();
+            }
+        }
+
+        private void SaveMerchantCategory()
+        {
+            merchantCategory = new MerchantCategory();
+            BindingHelper.RequestToObject(merchantCategory);
+            iMerchantCategoryRepository.SaveOrUpdate(merchantCategory);
+
+            AjaxResult result = new AjaxResult();
+            result.statusCode = "200";
+            result.message = "添加成功";
+            result.navTabId = "referer";
+            result.callbackType = "closeCurrent";
+            FormatJsonResult jsonResult = new FormatJsonResult();
+            jsonResult.Data = result;
+            Response.Write(jsonResult.FormatResult());
+            Response.End();
+
+
+
+        }
+
+    }
+}
