5f4e3b9db17861d2b83f8507371e1a7d02e0ab87
wanghaichuan <haichuan300@163.com>
2013-02-20 10:55:04 +0800

    delete unused file merchantCategoryList

--- Friday.mvc/weblogin/rent/pAddRent.aspx.designer.cs
+++ Friday.mvc/weblogin/rent/pAddRent.aspx.designer.cs
@@ -67,13 +67,13 @@ namespace Friday.mvc.weblogin.rent {
         protected global::System.Web.UI.HtmlControls.HtmlInputText ShopHours;
         
         /// <summary>
-        /// Distance 控件。
+        /// Text1 控件。
         /// </summary>
         /// <remarks>
         /// 自动生成的字段。
         /// 若要进行修改，请将字段声明从设计器文件移到代码隐藏文件。
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputText Distance;
+        protected global::System.Web.UI.HtmlControls.HtmlInputText Text1;
         
         /// <summary>
         /// Rate 控件。
