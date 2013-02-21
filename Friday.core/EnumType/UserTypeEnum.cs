f4847ef71d173933c576ec17f7feae7b8920d676
wanghaichuan <haichuan300@163.com>
2013-02-21 19:43:19 +0800

    Add pLoginUserList.aspx

--- Friday.core/EnumType/UserTypeEnum.cs
+++ Friday.core/EnumType/UserTypeEnum.cs
@@ -2,22 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using friday.core.components;
 
 namespace friday.core.EnumType
 {
 
     public enum UserTypeEnum
     {
-        管理员=0,
+        [EnumDescription("管理员")]
+          管理员=0,
+        [EnumDescription("顾客")]
           顾客=1,
+        [EnumDescription("商店")]
           商店=2,
+        [EnumDescription("餐馆")]
           餐馆=3,
+        [EnumDescription("租房")]
           租房=4,
-        送货员=5,
-    商铺店小二=6,
-    餐馆店小二=7,
-    租房店小二=8,
-      会员用户=9
+        [EnumDescription("送货员")]
+          送货员=5,
+        [EnumDescription("商铺店小二")]
+          商铺店小二=6,
+        [EnumDescription("餐馆店小二")]
+          餐馆店小二=7,
+        [EnumDescription("租房店小二")]
+          租房店小二=8,
+        [EnumDescription("会员用户")]
+          会员用户=9
 
     }
 }
---
+++ Friday.mvc/weblogin/loginUser/pLoginUserList.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using friday.core.repositories;
+using friday.core;
+using friday.core.components;
+using friday.core.domain;
+
+namespace Friday.mvc.weblogin.loginUser
+{
+    public partial class pLoginUserList : System.Web.UI.Page
+    {
+        protected long total;
+        protected int pageNum;
+        protected int numPerPageValue;
+
+        protected string loginName;
+        protected string name;
+        protected string tel;
+        protected string email;
+
+        IRepository<LoginUser> iRepositoryLoginUser = UnityHelper.UnityToT<IRepository<LoginUser>>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Params["flag"] == "alldelete")
+            {
+                DeleteLoginUser();
+
+            }
+            else
+            {
+                numPerPageValue = Request.Form["numPerPage"] == null ? 10 : Convert.ToInt32(Request.Form["numPerPage"].ToString());
+                pageNum = Request.Form["pageNum"] == null ? 1 : Convert.ToInt32(Request.Form["pageNum"].ToString());
+                int start = (pageNum - 1) * numPerPageValue;
+                int limit = numPerPageValue;
+
+
+                IList<LoginUser> loginUserList = iRepositoryLoginUser.GetPageList(start, limit, out total);
+
+                repeater.DataSource = loginUserList;
+                repeater.DataBind();
+
+                numPerPage.Value = numPerPageValue.ToString();
+            }
+        }
+
+        private void DeleteLoginUser()
+        {
+            iRepositoryLoginUser.Delete(Request.Params["uid"]);
+
+            AjaxResult result = new AjaxResult();
+            result.statusCode = "200";
+            result.message = "修改成功";
+            FormatJsonResult jsonResult = new FormatJsonResult();
+            jsonResult.Data = result;
+            Response.Write(jsonResult.FormatResult());
+            Response.End();
+        }
+    }
+}
