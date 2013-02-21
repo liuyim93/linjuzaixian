56d8e716c4880c862327a3735e33731cfcb47174
wanghaichuan <haichuan300@163.com>
2013-02-21 13:32:06 +0800

    Add Friday.core.components.PictureUpload.cs

---
+++ Friday.core/components/PictureUpload.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.IO;
+
+namespace friday.core.components
+{
+    public static class  PictureUpload
+    {
+        public static string UploadImage(HttpFileCollection files,string ParentPath)
+        {
+            string fileoldName = "";
+            string fileExtension;
+            string filesnewName = "";
+            Random R = new Random();//创建产生随机数
+            try
+            {
+                for (int iFile = 0; iFile < files.Count; iFile++)
+                {
+                    HttpPostedFile postedFile = files[iFile];
+                    fileoldName = System.IO.Path.GetFileName(postedFile.FileName);
+                    if (!string.IsNullOrEmpty(fileoldName))
+                    {
+                        fileExtension = System.IO.Path.GetExtension(fileoldName).ToLower();
+
+                        int val = 10 + R.Next(999);//产生随机数为99以内任意
+                        int val1 = 10 + R.Next(999);//产生随机数为999以内任意
+                        filesnewName = DateTime.Now.ToString("yyyyMMddHHmmss") + val.ToString() + val1.ToString() + fileExtension;
+                        if (!string.IsNullOrEmpty(filesnewName))
+                        {
+                            File.Delete(System.Web.HttpContext.Current.Request.MapPath("~/weblogin/uploadimage/" + ParentPath) + filesnewName);
+                        }
+                        postedFile.SaveAs(System.Web.HttpContext.Current.Request.MapPath("~/weblogin/uploadimage/" + ParentPath) + filesnewName);
+                    }
+                }
+            }
+            catch (System.Exception Ex)
+            {
+            }
+            
+            return filesnewName;
+        }
+    }
+}
--- Friday.mvc/weblogin/food/pEditFood.aspx.cs
+++ Friday.mvc/weblogin/food/pEditFood.aspx.cs
@@ -42,40 +42,12 @@ namespace Friday.mvc.weblogin
 
             BindingHelper.RequestToObject(f);
 
-            string fileoldName = "";
-            string fileExtension;
-            string filesnewName = "";
-            Random R = new Random();//创建产生随机数
-            HttpFileCollection files = HttpContext.Current.Request.Files;
-            try
-            {
-                for (int iFile = 0; iFile < files.Count; iFile++)
-                {
-                    HttpPostedFile postedFile = files[iFile];
-                    fileoldName = System.IO.Path.GetFileName(postedFile.FileName);
-                    if (!string.IsNullOrEmpty(fileoldName))
-                    {
-                        fileExtension = System.IO.Path.GetExtension(fileoldName).ToLower();
-
-                        int val = 10 + R.Next(999);//产生随机数为99以内任意
-                        int val1 = 10 + R.Next(999);//产生随机数为999以内任意
-                        filesnewName = DateTime.Now.ToString("yyyyMMddHHmmss") + val.ToString() + val1.ToString() + fileExtension;
-                        if (!string.IsNullOrEmpty(filesnewName))
-                        {
-                            File.Delete(System.Web.HttpContext.Current.Request.MapPath("~/uploadimage/foodImage/") + filesnewName);
-                        }
-                        postedFile.SaveAs(System.Web.HttpContext.Current.Request.MapPath("~/uploadimage/foodImage/") + filesnewName);
-                    }
-                }
-            }
-            catch (System.Exception Ex)
-            {
-            }
+            string filesnewName = PictureUpload.UploadImage(HttpContext.Current.Request.Files,"foodImage/");
             if (!string.IsNullOrEmpty(filesnewName))
             {
                 f.Image = "/uploadimage/foodImage/" + filesnewName;
             }
-            string foodType;
+            //string foodType;
             //ShopFoodTypeRepository shopFoodTypeR = new ShopFoodTypeRepository();
             //if (string.IsNullOrEmpty(Type.Value))
             //{
