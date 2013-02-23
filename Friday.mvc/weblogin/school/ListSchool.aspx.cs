f5e28b7fe5aa0016316e6a2beaee258d4c3015b7
pangfuxing <pang_fu_xing@126.com>
2013-02-23 11:45:15 +0800

    change  ListSchool's place

---
+++ Friday.mvc/weblogin/school/ListSchool.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using friday.core.domain;
+using friday.core.repositories;
+using friday.core;
+using friday.core.components;
+
+namespace Friday.mvc.weblogin
+{
+    public partial class ListSchool : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IRepository<School> reposch = UnityHelper.UnityToT<IRepository<School>>();
+            IList<School> schs = new List<School>();
+            schs = reposch.GetAll();
+            //if (parentDataDictionary != null)
+            //{
+            //    IList<DataDictionary> DataDictionarys = DataDictionaryRep.GetDataDictionaryFromParentID(parentDataDictionary.TreeCode);
+            repeater.DataSource = schs;
+            repeater.DataBind();
+            //}
+        }
+    }
+}
