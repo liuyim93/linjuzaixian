c6d897daee2b7c6874f0362e56946af30bd3be73
basilwang <ocam@163.com>
2013-02-25 14:45:32 +0800

    add SystemFunctionObjectService ,still can't compile

---
+++ Friday.core/domain/SystemFunctionObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.domain;
+
+namespace friday.core
+{
+    public class SystemFunctionObject:Entity
+    {
+        public virtual string PermissonTag
+        {
+            get;
+            set;
+        }
+        public virtual string FunctionObjectName
+        {
+            get;
+            set;
+        }
+        public virtual string ParentFunctionObjectId
+        {
+            get;
+            set;
+        }
+        public virtual bool FunctionAvailable
+        {
+            get;
+            set;
+        }
+        public virtual bool DeletePermissionAvailable
+        {
+            get;
+            set;
+        }
+        public virtual bool EditPermissionAvailable
+        {
+            get;
+            set;
+        }
+    }
+}
