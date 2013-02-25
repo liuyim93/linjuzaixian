7d2871c4dbe05d492262782fc4233397b6e76379
wanghaichuan <haichuan300@163.com>
2013-02-25 16:56:56 +0800

    Add pEditMyHouseOrder.aspx

---
+++ Friday.mvc/weblogin/myHouseOrder/pEditMyHouseOrder.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using friday.core.repositories;
+using friday.core;
+using friday.core.domain;
+using friday.core.components;
+
+namespace Friday.mvc.weblogin.myHouseOrder
+{
+    public partial class pEditMyHouseOrder : System.Web.UI.Page
+    {
+        IRepository<MyHouseOrder> iMyHouseOrderRepository = UnityHelper.UnityToT<IRepository<MyHouseOrder>>();
+        IRepository<SystemUser> iSystemUserRepository = UnityHelper.UnityToT<IRepository<SystemUser>>();
+
+        private MyHouseOrder myHouseOrder;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string uid = Request.Params["uid"].ToString();
+            myHouseOrder = iMyHouseOrderRepository.Load(uid);
+            if (Request.Params["__EVENTVALIDATION"] != null)
+            {
+
+                SaveMyHouseOrder();
+            }
+            else
+            {
+                BindingHelper.ObjectToControl(myHouseOrder, this);
+                SystemUser.Value = myHouseOrder.SystemUser.LoginUser.LoginName;
+                SystemUserID.Value = myHouseOrder.SystemUser.Id;
+            }
+        }
+
+        private void SaveMyHouseOrder()
+        {
+            BindingHelper.RequestToObject(myHouseOrder);
+            myHouseOrder.SystemUser = iSystemUserRepository.Get(Request.Params["SystemUserID"]);
+
+            iMyHouseOrderRepository.SaveOrUpdate(myHouseOrder);
+
+            AjaxResult result = new AjaxResult();
+            result.statusCode = "200";
+            result.message = "修改成功";
+            result.navTabId = "referer";
+            result.callbackType = "closeCurrent";
+            FormatJsonResult jsonResult = new FormatJsonResult();
+            jsonResult.Data = result;
+            Response.Write(jsonResult.FormatResult());
+            Response.End();
+
+        }
+
+    }
+}
