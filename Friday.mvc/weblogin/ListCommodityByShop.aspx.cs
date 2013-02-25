b1351203b182f41da8a1e813414e5848e21bd65a
pangfuxing <pang_fu_xing@126.com>
2013-02-25 17:07:53 +0800

    Add ListCommoditByShop

---
+++ Friday.mvc/weblogin/ListCommodityByShop.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using friday.core.repositories;
+using friday.core;
+using friday.core.components;
+using friday.core.domain;
+
+namespace Friday.mvc.weblogin
+{
+    public partial class ListCommodityByShop : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IRepository<Shop> repoShop = UnityHelper.UnityToT<IRepository<Shop>>();
+            ICommodityRepository repoCommodity = UnityHelper.UnityToT<ICommodityRepository>();
+
+            IList<Commodity> commoditys = new List<Commodity>();
+
+            string shop_id;
+
+            if (Request.Form["shop_id"] != null)
+            {
+                shop_id = Request.Form["shop_id"];
+            }
+            else
+            {
+                shop_id = Request.Params["shop_id"];
+            }
+
+            List<DataFilter> dfl = new List<DataFilter>();
+
+            if (!string.IsNullOrEmpty(shop_id))
+            {
+                dfl.Add(new DataFilter() { type = "Shop", value = shop_id });
+            }
+
+            dfl.Add(new DataFilter() { type = "IsDelete"});
+
+            commoditys = repoCommodity.Search(dfl);
+            repeater.DataSource = commoditys;
+            repeater.DataBind();
+        }
+    }
+}
