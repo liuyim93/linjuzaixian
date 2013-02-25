dc7b5d20caa33683c06efc0777624957f0624238
pangfuxing <pang_fu_xing@126.com>
2013-02-25 10:36:43 +0800

    Update Test_RestaurantMultiSearch()

--- Friday.Test2/Test_RestaurantMultiSearch.cs
+++ Friday.Test2/Test_RestaurantMultiSearch.cs
@@ -6,6 +6,8 @@ using friday.core.domain;
 using friday.core.repositories;
 using friday.core;
 using friday.core.EnumType;
+using friday.core.components;
+using System.Linq;
 
 namespace Friday.Test2
 {
@@ -15,6 +17,10 @@ namespace Friday.Test2
         [Test]
         public void Test()
         {
+            string shopName = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string loginName = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string shopName2 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string loginName2 = Guid.NewGuid().ToString().GetHashCode().ToString();
             IRestaurantRepository iRestaurantRepository = UnityHelper.UnityToT<IRestaurantRepository>();
             IList<Restaurant> iRestaurants = new List<Restaurant>();
             ILoginUserOfMerchantRepository iLoginUserOfMerchantRepository = UnityHelper.UnityToT<ILoginUserOfMerchantRepository>();
@@ -32,8 +38,8 @@ namespace Friday.Test2
                Distance = "100",
                Email = "222@qq.com",
                Logo = "image/21222.jpg",
-               Name = "天外村酒楼",
-               Owener = "刘德华",
+               Name = shopName,
+               Owener = loginName,
                ShortName = "tianwaicun",
                Tel = "18799999992",
                SendTime = 30,
@@ -45,8 +51,8 @@ namespace Friday.Test2
             iRestaurantRepository.SaveOrUpdate(rest1);
 
            LoginUser lu1 = new LoginUser();
-           lu1.LoginName = "LogLDH" + rest1.Owener;
-           lu1.Password = "LPass" + rest1.Tel;
+           lu1.LoginName = loginName;
+           lu1.Password = loginName;
            lu1.UserType = UserTypeEnum.餐馆;
            iLoginUserRepository.SaveOrUpdate(lu1);
 
@@ -56,23 +62,23 @@ namespace Friday.Test2
            lum.LoginUser = lu1;
            iLoginUserOfMerchantRepository.SaveOrUpdate(lum);
 
-           //餐馆1的店小二   2个
-           for (int i = 0; i < 2; i++) 
-           {          
-               LoginUser xiaoErLoginUser = new LoginUser()
-              {
-               IsAdmin = false,
-               LoginName = i+"号小二" + rest1.Name,
-               Password = i+"haoxiaoer" + rest1.Tel,
-               UserType = UserTypeEnum.餐馆店小二,
-              };
-             iLoginUserRepository.SaveOrUpdate(xiaoErLoginUser);
+           ////餐馆1的店小二   2个
+           //for (int i = 0; i < 2; i++) 
+           //{          
+           //    LoginUser xiaoErLoginUser = new LoginUser()
+           //   {
+           //    IsAdmin = false,
+           //    LoginName = i+"号小二" + rest1.Name,
+           //    Password = i+"haoxiaoer" + rest1.Tel,
+           //    UserType = UserTypeEnum.餐馆店小二,
+           //   };
+           //  iLoginUserRepository.SaveOrUpdate(xiaoErLoginUser);
  
-             LoginUserOfMerchant xiaoerlum = new LoginUserOfMerchant();
-             xiaoerlum.Merchant = rest1;
-             xiaoerlum.LoginUser = xiaoErLoginUser;
-             iLoginUserOfMerchantRepository.SaveOrUpdate(xiaoerlum);
-            }
+           //  LoginUserOfMerchant xiaoerlum = new LoginUserOfMerchant();
+           //  xiaoerlum.Merchant = rest1;
+           //  xiaoerlum.LoginUser = xiaoErLoginUser;
+           //  iLoginUserOfMerchantRepository.SaveOrUpdate(xiaoerlum);
+           // }
 
 
            Restaurant rest2 = new Restaurant()
@@ -87,8 +93,8 @@ namespace Friday.Test2
                Distance = "200",
                Email = "pang@126.com",
                Logo = "image/haiyang.jpg",
-               Name = "银座中豪大酒店",
-               Owener = "任贤齐",
+               Name = shopName2,
+               Owener = loginName2,
                ShortName = "yinzuo",
                Tel = "152785138650",
                SendTime = 30,
@@ -100,8 +106,8 @@ namespace Friday.Test2
            iRestaurantRepository.SaveOrUpdate(rest2);
 
            LoginUser lu2 = new LoginUser();
-           lu2.LoginName = "LognRxq" + rest2.Owener;
-           lu2.Password = "LPass" + rest2.Tel;
+           lu2.LoginName = loginName2;
+           lu2.Password = loginName2;
            lu2.UserType = UserTypeEnum.餐馆;
            iLoginUserRepository.SaveOrUpdate(lu2);
 
@@ -111,23 +117,54 @@ namespace Friday.Test2
            lum2.LoginUser = lu2;
            iLoginUserOfMerchantRepository.SaveOrUpdate(lum2);
 
-           //餐馆2的店小二   2个
-           for (int i = 0; i < 2; i++)
-           {
-               LoginUser xiaoErLoginUser2 = new LoginUser()
-               {
-                   IsAdmin = false,
-                   LoginName = i + "号" + rest2.Name,
-                   Password = i + "haoxiaoer" + rest2.Tel,
-                   UserType = UserTypeEnum.餐馆店小二,
-               };
-               iLoginUserRepository.SaveOrUpdate(xiaoErLoginUser2);
+           ////餐馆2的店小二   2个
+           //for (int i = 0; i < 2; i++)
+           //{
+           //    LoginUser xiaoErLoginUser2 = new LoginUser()
+           //    {
+           //        IsAdmin = false,
+           //        LoginName = i + "号" + rest2.Name,
+           //        Password = i + "haoxiaoer" + rest2.Tel,
+           //        UserType = UserTypeEnum.餐馆店小二,
+           //    };
+           //    iLoginUserRepository.SaveOrUpdate(xiaoErLoginUser2);
 
-               LoginUserOfMerchant xiaoerlum2 = new LoginUserOfMerchant();
-               xiaoerlum2.Merchant = rest2;
-               xiaoerlum2.LoginUser = xiaoErLoginUser2;
-               iLoginUserOfMerchantRepository.SaveOrUpdate(xiaoerlum2);
-           }
+           //    LoginUserOfMerchant xiaoerlum2 = new LoginUserOfMerchant();
+           //    xiaoerlum2.Merchant = rest2;
+           //    xiaoerlum2.LoginUser = xiaoErLoginUser2;
+           //    iLoginUserOfMerchantRepository.SaveOrUpdate(xiaoerlum2);
+           //}
+           List<DataFilter> filterList = new List<DataFilter>();
+           List<DataFilter> loginUserOfMechentList = new List<DataFilter>();
+           List<DataFilter> loginUserList = new List<DataFilter>();
+           loginUserList.Add(new DataFilter()
+           {
+               type = "LoginName",
+               value = loginName
+
+           });
+           //loginUserList.Add(new DataFilter()
+           //{
+           //    type = "UserType",
+           //    value = "餐馆"
+
+           //});
+           loginUserOfMechentList.Add(new DataFilter()
+           {
+               type = "LoginUser",
+               field = loginUserList
+
+           });
+
+           filterList.Add(new DataFilter()
+           {
+               type = "LoginUserOfMechant",
+               field = loginUserOfMechentList
+           });
+
+           Restaurant r = iRestaurantRepository.Search(filterList).FirstOrDefault();
+          
+           Assert.IsTrue(r.Name==shopName,string.Format("餐馆名字实际结果：{0}与期望结果{1}不一致",r.Name,shopName));
         }
 
      
