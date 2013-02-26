9b621fb224c76d89cdfa158cd05400210872b425
basilwang <ocam@163.com>
2013-02-26 15:17:44 +0800

    have problems!!

--- Friday.mvc/weblogin/permission/pSystemFunctionObjectDetail.aspx.cs
+++ Friday.mvc/weblogin/permission/pSystemFunctionObjectDetail.aspx.cs
@@ -7,31 +7,56 @@ using System.Web.UI.WebControls;
 using friday.core.repositories;
 using friday.core;
 using friday.core.domain;
+using friday.core.components;
 
 namespace Friday.mvc.weblogin
 {
     public partial class pSystemFunctionObjectDetail : System.Web.UI.Page
     {
+        SystemFunctionObjectInRole systemFunctionObjectInRole;
         ISystemFunctionObjectRepository iSystemFunctionObjectRepository = UnityHelper.UnityToT<ISystemFunctionObjectRepository>();
         ISystemFunctionObjectInRoleRepository iSystemFunctionObjectInRoleRepository = UnityHelper.UnityToT<ISystemFunctionObjectInRoleRepository>();
         protected void Page_Load(object sender, EventArgs e)
         {
-           string uid = Request.Params["uid"];
-           string rid = Request.Params["rid"];
-           SystemFunctionObject systemFunctionObject= iSystemFunctionObjectRepository.Get(uid);
+            string uid = Request.Params["uid"];
+            string rid = Request.Params["rid"];
+            SystemFunctionObject systemFunctionObject = iSystemFunctionObjectRepository.Get(uid);
+            systemFunctionObjectInRole = iSystemFunctionObjectInRoleRepository.Get(rid, uid);
+   
+            if (Request.Params["__EVENTVALIDATION"] != null)
+            {
+                SaveOrUpdate();
+            }
            if (systemFunctionObject != null)
            {
                this.panelEnabledState.Visible = systemFunctionObject.FunctionAvailable;
                this.panelEditableState.Visible  = systemFunctionObject.EditPermissionAvailable;
                this.panelDeletableState.Visible = systemFunctionObject.DeletePermissionAvailable;
            }
-           SystemFunctionObjectInRole systemFunctionObjectInRole = iSystemFunctionObjectInRoleRepository.Get(rid, uid);
-           if (systemFunctionObjectInRole!=null)
+          if (systemFunctionObjectInRole!=null)
            {
                this.cbEnabledState.Checked = systemFunctionObjectInRole.Enabled;
                this.cbEditableState.Checked = systemFunctionObjectInRole.Editable;
                this.cbDeletableState.Checked = systemFunctionObjectInRole.Deletable;
            }
+        }
+        private void SaveOrUpdate()
+        {
+            systemFunctionObjectInRole.Enabled = this.cbEnabledState.Checked;
+            systemFunctionObjectInRole.Editable = this.cbEditableState.Checked;
+            systemFunctionObjectInRole.Deletable = this.cbDeletableState.Checked;
+            iSystemFunctionObjectInRoleRepository.SaveOrUpdate(systemFunctionObjectInRole);
+
+            AjaxResult result = new AjaxResult();
+            result.statusCode = "200";
+            result.message = "添加成功";
+            result.navTabId = "referer";
+            result.callbackType = "closeCurrent";
+            FormatJsonResult jsonResult = new FormatJsonResult();
+            jsonResult.Data = result;
+            Response.Write(jsonResult.FormatResult());
+            Response.End();
+
         }
     }
 }
