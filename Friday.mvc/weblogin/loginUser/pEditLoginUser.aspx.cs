9f596fa7ca9a527a48cd7bcedede485a49906d8c
wanghaichuan <haichuan300@163.com>
2013-02-26 15:58:00 +0800

    pEditLoginUser.aspx

--- Friday.mvc/weblogin/loginUser/pEditLoginUser.aspx.cs
+++ Friday.mvc/weblogin/loginUser/pEditLoginUser.aspx.cs
@@ -14,12 +14,15 @@ namespace Friday.mvc.weblogin
     public partial class pEditLoginUser : System.Web.UI.Page
     {
         IRepository<LoginUser> iLoginUserRepository = UnityHelper.UnityToT<IRepository<LoginUser>>();
+        IUserInRoleRepository iUserInRoleRepository = UnityHelper.UnityToT<IUserInRoleRepository>();
+        IRepository<SystemRole> iSystemRoleRepository = UnityHelper.UnityToT<IRepository<SystemRole>>();
 
         private LoginUser loginUser;
+        private string uid;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            string uid = Request.Params["uid"].ToString();
+            uid = Request.Params["uid"].ToString();
             loginUser = iLoginUserRepository.Load(uid);
             if (Request.Params["__EVENTVALIDATION"] != null)
             {
@@ -30,6 +33,11 @@ namespace Friday.mvc.weblogin
             {
                 BindingHelper.ObjectToControl(loginUser, this);
                 IsAdminV.Value = (loginUser.IsAdmin == true ? "是" : "否");
+
+
+                string[] schofmntname = iUserInRoleRepository.GetRoleNamesAndIDByLoginUserID(uid);
+                this.NameSet.Value = schofmntname[0];
+                this.IDSet.Value = schofmntname[1];
             }
         }
 
@@ -39,6 +47,23 @@ namespace Friday.mvc.weblogin
             BindingHelper.RequestToObject(loginUser);
             loginUser.IsAdmin = (IsAdminV.Value == "是" ? true : false);
             iLoginUserRepository.SaveOrUpdate(loginUser);
+
+            iUserInRoleRepository.DeleteUserInRoleByLoginUserID(uid);
+            string roleID = "";
+            if (this.IDSet.Value != null && this.IDSet.Value != "")
+            {
+                roleID = this.IDSet.Value;
+                string[] sArray = roleID.Split(',');
+
+                foreach (string aid in sArray)
+                {
+                    UserInRole userInRole = new UserInRole();
+                    userInRole.Role = iSystemRoleRepository.Get(aid);
+                    userInRole.LoginUser = loginUser;
+                    iUserInRoleRepository.SaveOrUpdate(userInRole);
+                }
+            }
+
 
             AjaxResult result = new AjaxResult();
             result.statusCode = "200";
