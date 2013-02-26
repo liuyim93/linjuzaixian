adc78edb35475f84d70d76459ca991a1c5d07317
pangfuxing <pang_fu_xing@126.com>
2013-02-26 20:59:58 +0800

    add FunctionObj

--- Friday.core/components/permission/FunctionObj.cs
+++ Friday.core/components/permission/FunctionObj.cs
@@ -10,23 +10,130 @@ namespace friday.core.services
     {
         public 餐馆管理 餐馆管理模块 { get; set; }
         public 统计 统计模块 { get; set; }
-        public 商铺管理 商铺管理模块 { get; set; }
+        public 商店管理 商店管理模块 { get; set; }
+        public 租房管理 租房管理模块 { get; set; }
+        public 顾客管理 顾客管理模块 { get; set; }
+        public 商家账号管理 商家账号管理模块 { get; set; }
+        public 学校信息管理 学校信息管理模块 { get; set; }
+        public 商品类型管理 商品类型管理模块 { get; set; }
+        public 商家活动管理 商家活动管理模块 { get; set; }
+
+        public 消息管理     消息管理模块 { get; set; }
+        public 反馈管理     反馈管理模块 { get; set; }
+
+        public 食品订单管理 食品订单管理模块 { get; set; }
+        public 租房订单管理 租房订单管理模块 { get; set; }
+        public 商品订单管理 商品订单管理模块 { get; set; }
+
+        public 角色权限管理 角色权限管理模块 { get; set; }
+        public 功能模块管理 功能模块管理模块 { get; set; }
+     
+
     }
     public class 餐馆管理 : BaseModel
     {
         [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
         public FunctionTag 餐馆维护 { get; set; }
-        [PermissionSetting(PermissionTag.Edit, PermissionTag.Delete)]
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
         public FunctionTag 菜品维护 { get; set; }
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 员工维护 { get; set; }
     }
-    public class 商铺管理 : BaseModel
+    public class 商店管理 : BaseModel
     {
         [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
-        public FunctionTag 商铺维护 { get; set; }
+        public FunctionTag 商店维护 { get; set; }
         [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable,PermissionTag.Delete)]
         public FunctionTag 商品维护 { get; set; }
         [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable,PermissionTag.Delete)]
         public FunctionTag 商铺经营类型维护 { get; set; }
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 员工维护 { get; set; }
+    }
+    public class 租房管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 租房维护 { get; set; }
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 房屋维护 { get; set; }
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 员工维护 { get; set; }
+    }
+    public class 顾客管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 顾客维护 { get; set; }
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable)]
+        public FunctionTag 密码重置 { get; set; }
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 顾客地址维护 { get; set; }
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 顾客收藏夹维护 { get; set; }
+    }
+    public class 商家账号管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 商家账号维护 { get; set; }
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable)]
+        public FunctionTag 密码重置 { get; set; }      
+    }
+    public class 学校信息管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 学校信息维护 { get; set; }        
+    }
+    public class 商品类型管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 商品类型维护 { get; set; }
+    }
+    public class 商家活动管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 商家活动维护 { get; set; }
+    }
+    public class 消息管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 消息维护 { get; set; }
+    }
+    public class 反馈管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 反馈维护 { get; set; }
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable)]
+        public FunctionTag 反馈回复 { get; set; }
+    }
+    public class 食品订单管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 食品订单维护 { get; set; }
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 订单明细维护 { get; set; }
+    }
+    public class 租房订单管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 租房订单维护 { get; set; }
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 订单明细维护 { get; set; }
+    }
+    public class 商品订单管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 商品订单维护 { get; set; }
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 订单明细维护 { get; set; }
+    }
+    public class 角色权限管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 角色权限维护 { get; set; }
+    }
+    public class 功能模块管理 : BaseModel
+    {
+        [PermissionSetting(PermissionTag.Edit, PermissionTag.Enable, PermissionTag.Delete)]
+        public FunctionTag 功能模块维护 { get; set; }
     }
     public class 统计 : BaseModel
     {
