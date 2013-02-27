cc77c4336e66511c856edbc5b9abf16011aaa0e7
basilwang <ocam@163.com>
2013-02-28 07:59:09 +0800

    add login.aspx

---
+++ Friday.mvc/weblogin/login.aspx
@@ -0,0 +1,58 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="login.aspx.cs" Inherits="friday.mvc.login" %>
+
+<!doctype html public "-//w3c//dtd html 4.01 transitional//en" "http://www.w3c.org/tr/1999/rec-html401-19991224/loose.dtd">
+<!-- saved from url=(0024)http://admin.518fax.net/ -->
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>管理员登录</title>
+    <link href="../Content/adminLogin.css" type="text/css" rel="stylesheet"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta content="MSHTML 6.00.6000.16809" name="GENERATOR"/>
+</head>
+<body id="userlogin_body">
+<form id="theform" name="theform" action="login.aspx?flag=login" method="post">
+
+<div id="panSiteFactory">
+<div id="siteFactoryLogin">
+<div id="user_login">
+<dl>
+  <dd id="user_top">
+  <ul>
+    <li class="user_top_l"></li>
+    <li class="user_top_c"></li>
+    <li class="user_top_r"></li></ul>
+  </dd>
+<dd id="user_main">
+  <ul>
+    <li class="user_main_l"></li>
+    <li class="user_main_c">
+    <div class="user_main_box">
+        <ul>
+          <li class="user_main_text">用户名： </li>
+          <li class="user_main_input"><input class="TxtUserNameCssClass" id="account"  maxlength="20" name="account"/> </li>
+        </ul>
+        <ul>
+          <li class="user_main_text">密 码： </li>
+          <li class="user_main_input"><input class="TxtPasswordCssClass" id="password" type="password" name="password"/></li>
+        </ul>
+
+        <ul style="display:none">
+          <li class="user_main_text">验证码： </li>
+          <li class="user_main_input"><input class="TxtYanzheng" id="userauthcode" name="userauthcode"/> </li>
+          <li>
+          <img id="imgVerify" src="/VerifyCode.aspx?" alt="看不清？点击更换" onclick="this.src=this.src+'?'"border="0" class="captcha" />
+          </li>
+        </ul>
+    </div>
+    </li>
+    <li class="user_main_r"><input type="submit" value="登录" /></li>
+    
+    
+  </ul>
+  </dd><dd id="user_bottom">
+  </dd>
+</dl>
+</div></div></div>
+</form>
+</body>
+</html>
---
+++ Friday.mvc/weblogin/login.aspx.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using friday.core.repositories;
+using friday.core;
+
+
+namespace friday.mvc
+{
+    public partial class login : System.Web.UI.Page
+    {
+        protected string message;
+        private string userid;
+        private ILoginUserRepository iLoginUserRepository = UnityHelper.UnityToT<ILoginUserRepository>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Params["flag"] == "login")
+            {
+                string adminLoginName = Request.Form["account"];
+
+                string psw = Request.Form["password"];
+
+                string code = Request.Form["userauthcode"];
+
+                string cookieCode = getCookieCode().Trim().ToLower();
+
+                if (!string.IsNullOrEmpty(code))
+                {
+                    code = code.ToLower().Trim();
+                }
+                ValidateResult vr = validateLogin(adminLoginName, psw, code, cookieCode);
+                if (vr.isSucceed)
+                {
+                    //2013-02-27 basilwang we need add checkbox for isPersistent
+                    populateFormAuthCookie(true, userid, "");
+                    Response.Redirect("index.aspx");
+                }
+                else
+                {
+                    Response.Write("<Script>alert('"+vr.message+"')</Script>");
+                }
+            }
+        }
+        private string getCookieCode()
+        {
+            HttpCookie cookie = Request.Cookies["weat_anonymous"];
+            string code = cookie.Values["cookieCode"];
+            if (string.IsNullOrEmpty(code))
+            {
+                return "1111";
+            }
+            return code;
+        }
+        protected void populateFormAuthCookie(bool isPersistent, string userID, string userType)
+        {
+            DateTime expiredTime;
+            if (isPersistent)
+            {
+                expiredTime = DateTime.Now.AddMonths(10);
+            }
+            else
+            {
+                expiredTime = DateTime.Now.AddMinutes(10);
+            }
+            FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1,
+                               userID,
+                               DateTime.Now,
+                               expiredTime,
+                               isPersistent,
+                               userType, //CompanyID
+                               FormsAuthentication.FormsCookiePath);
+            // Encrypt the ticket.
+            string encTicket = FormsAuthentication.Encrypt(ticket);//加密序列化验证票为字符串
+            //生成Cookie
+            if (isPersistent)
+            {
+                //2011-04-24  统一bpm和dwz验证，重新处理了跳票超时问题的bug
+                HttpCookie cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encTicket)
+                {
+                    HttpOnly = true,
+                    Path = ticket.CookiePath,
+
+                    Expires = expiredTime,
+                    Domain = FormsAuthentication.CookieDomain,
+                };
+                //2012-12-31 basilwang can't user HttpContextBase for cookie , weird!! solved now
+                Response.Cookies.Add(cookie); //输出Cookie
+            }
+            else
+            {
+                //2012-12-30 basilwang FormsAuthentication.RedirectFromLoginPage will call SetAuthCookie,so we put SetAuthCookie here.
+                //and we don't use FormsAuthentication.RedirectFromLoginPage anymore
+                FormsAuthentication.SetAuthCookie(userID, false);
+            }
+ 
+        }
+        public class ValidateResult
+        {
+            public bool isSucceed;
+            public string message;
+        }
+        private ValidateResult validateLogin(string account, string psw, string code, string cookiecode)
+        {
+
+            ValidateResult vr = new ValidateResult();
+            if (!string.IsNullOrEmpty(account) && !string.IsNullOrEmpty(psw))
+            {
+                //2013-02-28 basilwang temporialiy block verify code
+                //if (!code.Equals(cookiecode))
+                //{
+                //    message = "验证码错误";
+                //    return false;
+                //}
+                //else
+                var loginUser = iLoginUserRepository.GetLoginUserByLoginName(account);
+                userid = loginUser.Id;
+                {
+                    if (psw==loginUser.Password)
+                    {
+                        vr.isSucceed = true;
+                    }
+                    else
+                    {
+                        vr.message = "验证失败";
+                        vr.isSucceed = false;
+                    }
+                }
+            }
+            else
+            {
+                vr.message = "用户名与密码不能为空";
+                vr.isSucceed = false;
+            }
+            return vr;
+        }
+    }
+}
