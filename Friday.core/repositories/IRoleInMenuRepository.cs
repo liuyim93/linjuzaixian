73e500d62b4888453bc410303eab8566cfc58ced
wanghaichuan <haichuan300@163.com>
2013-02-27 18:07:03 +0800

    Add RoleInMenuRepository.cs

---
+++ Friday.core/repositories/IRoleInMenuRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace friday.core.repositories
+{
+    public interface IRoleInMenuRepository : IRepository<RoleInMenu>    
+    {
+        bool GetRoleInMenuByMenuIDandRoleID(string menuid, string roleid);
+        IList<RoleInMenu> GetSystemMenuPListByRoleID(string RoleID);
+    }
+}
---
+++ Friday.core/repositories/RoleInMenuRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace friday.core.repositories
+{
+    public class RoleInMenuRepository : Repository<RoleInMenu>, IRoleInMenuRepository
+    {
+        public bool GetRoleInMenuByMenuIDandRoleID(string menuid, string roleid)
+        {
+            var query = Session.CreateQuery(@"select ur from RoleInMenu ur 
+                                                    left join fetch ur.Menu u 
+                                                    left join fetch ur.Role r 
+                                                where ur.Menu=:MenuID and ur.Role=:RoleID")
+                         .SetString("MenuID", menuid).SetString("RoleID", roleid)
+                         .List<RoleInMenu>().ToList<RoleInMenu>();
+            return ((query.Count > 0) ? true : false);
+
+        }
+        //根据角色找出菜单
+        public IList<RoleInMenu> GetSystemMenuPListByRoleID(string RoleID)
+        {
+
+            var query = Session.CreateQuery(@"select c from RoleInMenu c 
+                                                   left join fetch c.Role as r  where
+                                             r.RoleID=:RoleID").SetString("RoleID", RoleID);
+            //.SetFirstResult(start)
+            //.SetMaxResults(limit);
+            //total = Session.CreateQuery(@"select count(distinct u) from SystemUserRole as u")
+            // .UniqueResult<long>();
+
+            return query.List<RoleInMenu>();
+        }
+ 
+    }
+}
