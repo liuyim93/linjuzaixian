a8aeabfa4dda87200715ee8d484ed3583c85b33d
basilwang <ocam@163.com>
2013-03-01 18:04:26 +0800

    small change

--- Friday.core/repositories/SystemRoleRepository.cs
+++ Friday.core/repositories/SystemRoleRepository.cs
@@ -22,17 +22,11 @@ namespace friday.core.repositories
 
         //    return q;
         //}
-        }
         public SystemRole GetRoleByName(string SystemRoleName)
         {
             var s = (from x in this.Session.Query<SystemRole>() select x).Where(o => o.Name == SystemRoleName).SingleOrDefault();
             return s;
         }
-        public SystemRole GetRoleByName(string SystemRoleName)
-        {
-            var s = (from x in this.Session.Query<SystemRole>() select x).Where(o => o.Name == SystemRoleName).SingleOrDefault();
-            return s;
-
         protected virtual ICriteria Query
         {
             get { return Session.CreateCriteria(typeof(SystemRole)); }
