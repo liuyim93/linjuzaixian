1e4698b075f94c6e4919ffe264fb87d43b1a173c
wanghaichuan <haichuan300@163.com>
2013-03-01 16:27:59 +0800

    Add RestaurantService.cs

---
+++ Friday.core/services/RestaurantService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.utils;
+using friday.core.repositories;
+
+namespace friday.core.services
+{
+    class RestaurantService:IRestaurantService
+    {
+        IRestaurantRepository iRestaurantRepository =UnityHelper.UnityToT<IRestaurantRepository>();
+        ILogger iLogger = UnityHelper.UnityToT<ILogger>();
+
+        public void Save(Restaurant restaurant)
+        {
+            iLogger.LogMessage("插入Restaurant数据，ID：" + restaurant.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iRestaurantRepository.SaveOrUpdate(restaurant);
+        }
+
+        public void Update(Restaurant restaurant)
+        {
+            iLogger.LogMessage("更新Restaurant数据，ID：" + restaurant.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iRestaurantRepository.SaveOrUpdate(restaurant);
+        }
+
+        public void Delete(string id)
+        {
+            iLogger.LogMessage("删除Restaurant数据，ID：" + id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iRestaurantRepository.Delete(id);
+        }
+
+    }
+}
