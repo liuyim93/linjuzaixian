d57ec5753a6186613124d1540e48a319a99ddc1f
wanghaichuan <haichuan300@163.com>
2013-03-03 18:07:28 +0800

    Add OrderOfFoodService.cs

---
+++ Friday.core/services/IOrderOfFoodService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.domain;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public interface IOrderOfFoodService
+    {
+        OrderOfFood Load(string id);
+        void Save(OrderOfFood orderOfFood);
+        void Update(OrderOfFood orderOfFood);
+        void Delete(string id);
+        IList<OrderOfFood> Search(List<DataFilter> termList);
+        IList<OrderOfFood> Search(List<DataFilter> termList, int start, int limit, out long total);
+    }
+}
---
+++ Friday.core/services/OrderOfFoodService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.repositories;
+using friday.core.utils;
+using friday.core.domain;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public class OrderOfFoodService : IOrderOfFoodService
+    {
+        private IOrderOfFoodRepository iOrderOfFoodRepository;
+        private ILogger iLogger;
+        public OrderOfFoodService(IOrderOfFoodRepository iOrderOfFoodRepository, ILogger iLogger)
+        {
+            this.iOrderOfFoodRepository = iOrderOfFoodRepository;
+            this.iLogger = iLogger;
+        }
+        public OrderOfFood Load(string id)
+        {
+            return iOrderOfFoodRepository.Load(id);
+        }
+
+        public void Save(OrderOfFood orderOfFood)
+        {
+            iLogger.LogMessage("插入OrderOfFood数据，ID：" + orderOfFood.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iOrderOfFoodRepository.SaveOrUpdate(orderOfFood);
+        }
+
+        public void Update(OrderOfFood orderOfFood)
+        {
+            iLogger.LogMessage("更新OrderOfFood数据，ID：" + orderOfFood.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iOrderOfFoodRepository.SaveOrUpdate(orderOfFood);
+        }
+
+        public void Delete(string id)
+        {
+            iLogger.LogMessage("删除OrderOfFood数据，ID：" + id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iOrderOfFoodRepository.Delete(id);
+        }
+
+        public IList<OrderOfFood> Search(List<DataFilter> termList)
+        {
+            return iOrderOfFoodRepository.Search(termList);
+        }
+
+        public IList<OrderOfFood> Search(List<DataFilter> termList, int start, int limit, out long total)
+        {
+            return iOrderOfFoodRepository.Search(termList, start, limit, out total);
+        }
+    }
+}
