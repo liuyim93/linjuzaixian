0ded6a7d531a56d761cdc7f0c6d999f4cdf686c6
wanghaichuan <haichuan300@163.com>
2013-03-03 17:51:15 +0800

    add MyFavoriteService.cs

---
+++ Friday.core/services/IMyFavoriteService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.domain;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public interface IMyFavoriteService
+    {
+        MyFavorite Load(string id);
+        void Save(MyFavorite myFavorite);
+        void Update(MyFavorite myFavorite);
+        void Delete(string id);
+        IList<MyFavorite> Search(List<DataFilter> termList);
+        IList<MyFavorite> Search(List<DataFilter> termList, int start, int limit, out long total);
+    }
+}
---
+++ Friday.core/services/MyFavoriteService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.repositories;
+using friday.core.utils;
+using friday.core.domain;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public class MyFavoriteService : IMyFavoriteService
+    {
+        private IMyFavoriteRepository iMyFavoriteRepository;
+        private ILogger iLogger;
+        public MyFavoriteService(IMyFavoriteRepository iMyFavoriteRepository, ILogger iLogger)
+        {
+            this.iMyFavoriteRepository = iMyFavoriteRepository;
+            this.iLogger = iLogger;
+        }
+        public MyFavorite Load(string id)
+        {
+            return iMyFavoriteRepository.Load(id);
+        }
+
+        public void Save(MyFavorite myFavorite)
+        {
+            iLogger.LogMessage("插入MyFavorite数据，ID：" + myFavorite.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iMyFavoriteRepository.SaveOrUpdate(myFavorite);
+        }
+
+        public void Update(MyFavorite myFavorite)
+        {
+            iLogger.LogMessage("更新MyFavorite数据，ID：" + myFavorite.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iMyFavoriteRepository.SaveOrUpdate(myFavorite);
+        }
+
+        public void Delete(string id)
+        {
+            iLogger.LogMessage("删除MyFavorite数据，ID：" + id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iMyFavoriteRepository.Delete(id);
+        }
+
+        public IList<MyFavorite> Search(List<DataFilter> termList)
+        {
+            return iMyFavoriteRepository.Search(termList);
+        }
+
+        public IList<MyFavorite> Search(List<DataFilter> termList, int start, int limit, out long total)
+        {
+            return iMyFavoriteRepository.Search(termList, start, limit, out total);
+        }
+    }
+}
