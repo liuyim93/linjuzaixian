28c7b7698d6f82c220a18b0dc1da0807a33d4d44
wanghaichuan <haichuan300@163.com>
2013-03-03 18:43:34 +0800

    Add SystemUserService.cs

---
+++ Friday.core/services/SystemUserService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.repositories;
+using friday.core.utils;
+using friday.core.domain;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public class SystemUserService : ISystemUserService
+    {
+        private ISystemUserRepository iSystemUserRepository;
+        private ILogger iLogger;
+        public SystemUserService(ISystemUserRepository iSystemUserRepository, ILogger iLogger)
+        {
+            this.iSystemUserRepository = iSystemUserRepository;
+            this.iLogger = iLogger;
+        }
+        public SystemUser Load(string id)
+        {
+            return iSystemUserRepository.Load(id);
+        }
+
+        public void Save(SystemUser systemUser)
+        {
+            iLogger.LogMessage("插入SystemUser数据，ID：" + systemUser.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iSystemUserRepository.SaveOrUpdate(systemUser);
+        }
+
+        public void Update(SystemUser systemUser)
+        {
+            iLogger.LogMessage("更新SystemUser数据，ID：" + systemUser.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iSystemUserRepository.SaveOrUpdate(systemUser);
+        }
+
+        public void Delete(string id)
+        {
+            iLogger.LogMessage("删除SystemUser数据，ID：" + id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iSystemUserRepository.Delete(id);
+        }
+
+
+        public IList<SystemUser> Search(List<DataFilter> termList)
+        {
+            return iSystemUserRepository.Search(termList);
+        }
+
+        public IList<SystemUser> Search(List<DataFilter> termList, int start, int limit, out long total)
+        {
+            return iSystemUserRepository.Search(termList, start, limit, out total);
+        }
+    }
+}
