afac207b0a71ac084445508ccc3dcc7ee7196ec3
wanghaichuan <haichuan300@163.com>
2013-03-03 18:17:00 +0800

    Add RoleInMenuService.cs

---
+++ Friday.core/services/IRoleInMenuService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace friday.core.services
+{
+    public interface IRoleInMenuService
+    {
+        RoleInMenu Load(string id);
+        void Save(RoleInMenu roleInMenu);
+        void Update(RoleInMenu roleInMenu);
+        void Delete(string id);
+        bool GetRoleInMenuByMenuIDandRoleID(string menuid, string roleid);
+        IList<RoleInMenu> GetSystemMenuPListByRoleID(string RoleID);
+    }
+}
---
+++ Friday.core/services/RoleInMenuService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.repositories;
+using friday.core.utils;
+
+namespace friday.core.services
+{
+    public class RoleInMenuService : IRoleInMenuService
+    {
+        private IRoleInMenuRepository iRoleInMenuRepository;
+        private ILogger iLogger;
+        public RoleInMenuService(IRoleInMenuRepository iRoleInMenuRepository, ILogger iLogger)
+        {
+            this.iRoleInMenuRepository = iRoleInMenuRepository;
+            this.iLogger = iLogger;
+        }
+        public RoleInMenu Load(string id)
+        {
+            return iRoleInMenuRepository.Load(id);
+        }
+
+        public void Save(RoleInMenu roleInMenu)
+        {
+            iLogger.LogMessage("插入RoleInMenu数据，ID：" + roleInMenu.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iRoleInMenuRepository.SaveOrUpdate(roleInMenu);
+        }
+
+        public void Update(RoleInMenu roleInMenu)
+        {
+            iLogger.LogMessage("更新RoleInMenu数据，ID：" + roleInMenu.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iRoleInMenuRepository.SaveOrUpdate(roleInMenu);
+        }
+
+        public void Delete(string id)
+        {
+            iLogger.LogMessage("删除RoleInMenu数据，ID：" + id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iRoleInMenuRepository.Delete(id);
+        }
+
+        public bool GetRoleInMenuByMenuIDandRoleID(string menuid, string roleid)
+        {
+            return iRoleInMenuRepository.GetRoleInMenuByMenuIDandRoleID(menuid, roleid);
+        }
+
+        public IList<RoleInMenu> GetSystemMenuPListByRoleID(string RoleID)
+        {
+            return iRoleInMenuRepository.GetSystemMenuPListByRoleID(RoleID);
+        }
+
+    }
+}
