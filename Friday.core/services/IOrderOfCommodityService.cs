75790b03b063bef17959a4b293eaf24150dcbcb0
pangfuxing <pang_fu_xing@126.com>
2013-03-04 16:13:20 +0800

    Add OrderOfCommodityService

---
+++ Friday.core/services/IOrderOfCommodityService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.components;
+using friday.core.domain;
+
+namespace friday.core.services
+{
+    public interface IOrderOfCommodityService
+    {
+        OrderOfCommodity Load(string id);
+        void Save(OrderOfCommodity orderOfCommodity);
+        void Update(OrderOfCommodity orderOfCommodity);
+        void Delete(string id);
+        OrderOfCommodity SearchByShortName(string name);
+        IList<OrderOfCommodity> Search(List<DataFilter> termList);
+        IList<OrderOfCommodity> Search(List<DataFilter> termList, int start, int limit, out long total);
+    }
+}
---
+++ Friday.core/services/OrderOfCommodityService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.utils;
+using friday.core.repositories;
+using friday.core.components;
+using friday.core.domain;
+
+namespace friday.core.services
+{
+    public class OrderOfCommodityService:IOrderOfCommodityService
+    {
+        private IOrderOfCommodityRepository iOrderOfCommodityRepository;
+        private ILogger iLogger;
+        public OrderOfCommodityService(IOrderOfCommodityRepository iOrderOfCommodityRepository, ILogger iLogger)
+        {
+            this.iOrderOfCommodityRepository = iOrderOfCommodityRepository;
+            this.iLogger = iLogger;
+        }
+        public OrderOfCommodity Load(string id)
+        {
+            return iOrderOfCommodityRepository.Load(id);
+        }
+
+        public void Save(OrderOfCommodity orderOfCommodity)
+        {
+            iLogger.LogMessage("插入OrderOfCommodity数据，ID：" + orderOfCommodity.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iOrderOfCommodityRepository.SaveOrUpdate(orderOfCommodity);
+        }
+
+        public void Update(OrderOfCommodity orderOfCommodity)
+        {
+            iLogger.LogMessage("更新OrderOfCommodity数据，ID：" + orderOfCommodity.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iOrderOfCommodityRepository.SaveOrUpdate(orderOfCommodity);
+        }
+
+        public void Delete(string id)
+        {
+            iLogger.LogMessage("删除OrderOfCommodity数据，ID：" + id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iOrderOfCommodityRepository.Delete(id);
+        }
+
+        public IList<OrderOfCommodity> Search(List<DataFilter> termList)
+        {
+            return iOrderOfCommodityRepository.Search(termList);
+        }
+
+        public IList<OrderOfCommodity> Search(List<DataFilter> termList, int start, int limit, out long total)
+        {
+            return iOrderOfCommodityRepository.Search(termList, start, limit, out total);
+        }
+    }
+}
