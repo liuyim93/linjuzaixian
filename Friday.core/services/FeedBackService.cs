a0c8245a4c3e94f732c9976de79e262800277989
pangfuxing <pang_fu_xing@126.com>
2013-03-04 11:52:22 +0800

    Update FeedBack

--- Friday.core/services/FeedBackService.cs
+++ Friday.core/services/FeedBackService.cs
@@ -5,29 +5,50 @@ using System.Text;
 using friday.core.domain;
 using friday.core.repositories;
 using friday.core.utils;
+using friday.core.components;
 
 namespace friday.core.services
 {
     public class FeedBackService:IFeedBackService
     {
-        IRepository<FeedBack> iFeedBackRepository;
-        ILogger iLogger;
-        public FeedBackService(IRepository<FeedBack> iFeedBackRepository, ILogger iLogger)
+       private IFeedBackRepository iFeedBackRepository;
+        private ILogger iLogger;
+        public FeedBackService(IFeedBackRepository iFeedBackRepository, ILogger iLogger)
         {
             this.iFeedBackRepository = iFeedBackRepository;
             this.iLogger = iLogger;
         }
-        public void Save(FeedBack feedback)
+        public FeedBack Load(string id)
         {
-            
+            return iFeedBackRepository.Load(id);
         }
-        public void Update(FeedBack feedback)
-        {
 
+        public void Save(FeedBack feedBack)
+        {
+            iLogger.LogMessage("插入FeedBack数据，ID：" + feedBack.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iFeedBackRepository.SaveOrUpdate(feedBack);
         }
-        public void Delete(FeedBack feedback)
-        {
 
+        public void Update(FeedBack feedBack)
+        {
+            iLogger.LogMessage("更新FeedBack数据，ID：" + feedBack.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iFeedBackRepository.SaveOrUpdate(feedBack);
+        }
+
+        public void Delete(string id)
+        {
+            iLogger.LogMessage("删除FeedBack数据，ID：" + id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iFeedBackRepository.Delete(id);
+        }
+
+        public IList<FeedBack> Search(List<DataFilter> termList)
+        {
+            return iFeedBackRepository.Search(termList);
+        }
+
+        public IList<FeedBack> Search(List<DataFilter> termList, int start, int limit, out long total)
+        {
+            return iFeedBackRepository.Search(termList, start, limit, out total);
         }
 
     }
--- Friday.core/services/IFeedBackService.cs
+++ Friday.core/services/IFeedBackService.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using friday.core.domain;
+using friday.core.components;
 
 namespace friday.core.services
 {
     public interface IFeedBackService
     {
-        void Save(FeedBack feedback);
-        void Update(FeedBack feedback);
-        void Delete(FeedBack feedback);
+        FeedBack Load(string id);
+        void Save(FeedBack feedBack);
+        void Update(FeedBack feedBack);
+        void Delete(string id);
+        IList<FeedBack> Search(List<DataFilter> termList);
+        IList<FeedBack> Search(List<DataFilter> termList, int start, int limit, out long total);
 
     }
 }
