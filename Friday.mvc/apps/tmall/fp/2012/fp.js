048570bcf26a78af91bfa3183a9787c311b35ab0
basilwang <ocam@163.com>
2013-03-05 20:43:53 +0800

    small changes

--- Friday.mvc/apps/tmall/fp/2012/fp.js
+++ Friday.mvc/apps/tmall/fp/2012/fp.js
@@ -6,21 +6,21 @@
  * To change this template use File | Settings | File Templates.
  */
 /*pub-1|2013-02-01 11:22:14*/
-KISSY.add("2012/fp",function (H, C, P) {
-        var A = H.DOM, Q = H.Event, K = H.UA;
-        var J = window, O = document, F = J.g_config;
-        function B()
+KISSY.add("2012/fp",function (_kissy, _slide2, _category) {
+        var _dom = _kissy.DOM, _event = _kissy.Event, _browser = _kissy.UA;
+        var _window = window, _document = document, _config = _window.g_config;
+        function FP()
         { this._init() }
-        H.augment(B,
+        _kissy.augment(FP,
             {
                 _init: function () {
-                    var D = this;
-                    D._initMain();
+                    var _fp = this;
+                    _fp._initMain();
                 },
                 _initMain: function () {
-                    new C();
+                    new _slide2();
                     //new P("#J_Category", { viewId: "#J_SubCategory", subViews: ".j_SubView", triggers: ".j_MenuItem", bottomCl: ".j_BottomMenu", dataUrl: "http://" + location.host + "/go/rgn/mfp2012/all-cat-asyn.php" });
                 }
 
-            }); return B
+            }); return FP
     }, { requires: ["2012/mods/slide2","2012/mods/category"] });
