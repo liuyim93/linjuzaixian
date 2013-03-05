9b2d47582b8f3d13e6c3ae53384a44c99059a6a3
pangfuxing <pang_fu_xing@126.com>
2013-03-05 21:19:18 +0800

    Add  aspx

---
+++ Friday.mvc/weblogin/restaurantStatistic/pRestaurantStatisticDetail.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using friday.core;
+using friday.core.repositories;
+using friday.core.components;
+using friday.core.domain;
+using friday.core.services;
+
+namespace Friday.mvc.weblogin.restaurantStatistic
+{
+    public partial class pRestaurantStatisticDetail : BasePage
+    {
+        IRestaurantStatisticService iRestaurantStatisticService = UnityHelper.UnityToT<IRestaurantStatisticService>();
+        private RestaurantStatistic restaurantStatistic;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            string uid = Request.Params["uid"].ToString();
+            restaurantStatistic = iRestaurantStatisticService.Load(uid);
+            BindingHelper.ObjectToControl(restaurantStatistic, this);
+
+            this.RestaurantName.Value = restaurantStatistic.Restaurant.Name;
+            this.RestaurantName.Value = restaurantStatistic.Restaurant.Restaurant.Name;
+
+
+
+        }
+    }
+}
---
+++ Friday.mvc/weblogin/restaurantStatistic/pRestaurantStatisticList.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using friday.core;
+using friday.core.repositories;
+using friday.core.domain;
+using friday.core.components;
+using friday.core.services;
+
+namespace Friday.mvc.weblogin.restaurantStatistic
+{
+    public partial class pRestaurantStatisticList : BasePage
+    {
+        protected long total;
+        protected int pageNum;
+        protected int numPerPageValue;
+        public string systemUserId;
+
+
+        protected string startDate;
+        protected string endDate;
+        protected string name;
+  
+ 
+       IRestaurantStatisticService iRestaurantStatisticService = UnityHelper.UnityToT<IRestaurantStatisticService>();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        
+            if (Request.Params["flag"] != "alldelete")
+            {
+                if (Request.Params["flag"] != "alldelete")
+                {
+                    numPerPageValue = Request.Form["numPerPage"] == null ? 10 : Convert.ToInt32(Request.Form["numPerPage"].ToString());
+                    pageNum = Request.Form["pageNum"] == null ? 1 : Convert.ToInt32(Request.Form["pageNum"].ToString());
+                    int start = (pageNum - 1) * numPerPageValue;
+                    int limit = numPerPageValue;
+
+                    List<DataFilter> filterList = new List<DataFilter>();
+                    List<DataFilter> restaurantList = new List<DataFilter>();
+                    
+
+                    if (!string.IsNullOrEmpty(Request.Form["Name"]))
+                        filterList.Add(new DataFilter()
+                        {
+                            type = "Name",
+                            value = name = Request.Form["Name"]
+
+                        });
+                          
+                    var filter = new DataFilter();
+                    if (!string.IsNullOrEmpty(Request.Form["StartDate"]))
+                    {
+                        filter.type = "CreateTime";
+                        filter.value = startDate = Request.Form["StartDate"];
+                        if (!string.IsNullOrEmpty(Request.Form["EndDate"]))
+                        {
+                            filter.valueForCompare = endDate = Request.Form["EndDate"];
+                        }
+                        filterList.Add(filter);
+                    }
+
+                    IList<RestaurantStatistic> restaurantStatisticList = iRestaurantStatisticService.Search(filterList, start, limit, out total);
+
+                    repeater.DataSource = restaurantStatisticList;
+                    repeater.DataBind();
+
+                    numPerPage.Value = numPerPageValue.ToString();
+                }
+            }
+
+            else
+            {               
+
+                DeleteRestaurantStatistic();
+
+            }
+
+        }
+
+
+
+
+        private void DeleteRestaurantStatistic()
+        {
+
+
+            iRestaurantStatisticService.Delete(Request.Params["uid"]);
+
+            AjaxResult result = new AjaxResult();
+            result.statusCode = "200";
+            result.message = "删除成功";
+            FormatJsonResult jsonResult = new FormatJsonResult();
+            jsonResult.Data = result;
+            Response.Write(jsonResult.FormatResult());
+            Response.End();
+        }
+
+    }
+}
