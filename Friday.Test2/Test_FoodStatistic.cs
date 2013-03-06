95194f527eb0880fe3c3b8fa97bfd2dc480133f7
pangfuxing <pang_fu_xing@126.com>
2013-03-06 21:41:06 +0800

    Add Test_FoodStatistic()

--- Friday.Test2/Test_FoodStatistic.cs
+++ Friday.Test2/Test_FoodStatistic.cs
@@ -5,7 +5,11 @@ using NUnit.Framework;
 using friday.core.domain;
 using friday.core.repositories;
 using friday.core;
+using friday.core.EnumType;
+using friday.core.components;
+using System.Linq;
 using System.Transactions;
+
 
 namespace Friday.Test2
 {
@@ -27,112 +31,118 @@ namespace Friday.Test2
         [Test]
         public void Test()
         {
-            IRepository<FoodStatistic> iFoodStatisticRepository = UnityHelper.UnityToT<IRepository<FoodStatistic>>();
+            IFoodStatisticRepository iFoodStatisticRepository = UnityHelper.UnityToT<IFoodStatisticRepository>();
             IRepository<Food> iFoodRepository = UnityHelper.UnityToT<IRepository<Food>>();
             IRepository<Restaurant> iRestaurantRepository = UnityHelper.UnityToT<IRepository<Restaurant>>();
             IRepository<MerchantGoodsType> iMerchantGoodsTypeRepository = UnityHelper.UnityToT<IRepository<MerchantGoodsType>>();
             IList<FoodStatistic> iFoodStatistics = new List<FoodStatistic>();
 
+            string rstName1 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string rstName2 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string mgName1 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string mgName2 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string fdName1 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string fdName2 = Guid.NewGuid().ToString().GetHashCode().ToString();
 
-            Restaurant rst = new Restaurant()
+            Restaurant rst1 = new Restaurant()
             {
-                Name = "银座超市",
+                Name = rstName1,
                  MerchantType=0
-
             };
-            iRestaurantRepository.SaveOrUpdate(rst);
+            iRestaurantRepository.SaveOrUpdate(rst1);
 
-            MerchantGoodsType mg = new MerchantGoodsType()
+            MerchantGoodsType mg1 = new MerchantGoodsType()
             {
-                Merchant = rst,
-                GoodsType = "体育用品"
+                Merchant = rst1,
+                GoodsType = mgName1
             };
-            iMerchantGoodsTypeRepository.SaveOrUpdate(mg);
+            iMerchantGoodsTypeRepository.SaveOrUpdate(mg1);
 
-            Food fd = new Food()
+
+
+            Restaurant rst2 = new Restaurant()
             {
-                Name = "羽毛球",
+                Name = rstName2,
+                MerchantType = 0
+            };
+            iRestaurantRepository.SaveOrUpdate(rst2);
+
+            MerchantGoodsType mg2 = new MerchantGoodsType()
+            {
+                Merchant = rst2,
+                GoodsType = mgName2
+            };
+            iMerchantGoodsTypeRepository.SaveOrUpdate(mg2);
+
+            Food fd1 = new Food()
+            {
+                Name = fdName1,
                 Amount = 100,
-                Restaurant = rst,
-                MerchantGoodsType = mg
+                Restaurant = rst1,
+                MerchantGoodsType = mg1
             };
-            iFoodRepository.SaveOrUpdate(fd);
+            iFoodRepository.SaveOrUpdate(fd1);
 
             Food fd2 = new Food()
             {
-                Name = "排球",
+                Name = fdName2,
                 Amount = 100,
-                Restaurant = rst,
-                MerchantGoodsType = mg
+                Restaurant = rst2,
+                MerchantGoodsType = mg2
             };
             iFoodRepository.SaveOrUpdate(fd2);
 
-            Food fd3 = new Food()
+            for (int i = 0; i < 10; i++) 
             {
-                Name = "乒乓球",
-                Amount = 100,
-                Restaurant = rst,
-                MerchantGoodsType = mg
-            };
-            iFoodRepository.SaveOrUpdate(fd3);
 
-            //FoodStatistic fs = new FoodStatistic()
-            //{
-            //     Food=fd,
-            //     Amount=10,
-            //      Year=2013,
-            //      Month=1,
-            //      Day=1
-            //};
-            //iFoodStatisticRepository.SaveOrUpdate(fs);
+                FoodStatistic fdstc1 = new FoodStatistic()
+                {
+                    Year = 2013,
+                    Month = 3,
+                    Day = i,
+                    AverageValuing = i,
+                    ValuingCount = i,
+                    Amount = i,
+                    Food = fd1
+                };
+                iFoodStatisticRepository.SaveOrUpdate(fdstc1);
+
+                FoodStatistic fdstc2 = new FoodStatistic()
+                {
+                    Year = 2013,
+                    Month = 3,
+                    Day = i,
+                    AverageValuing = i,
+                    ValuingCount = i,
+                    Amount = i,
+                    Food = fd2
+                };
+                iFoodStatisticRepository.SaveOrUpdate(fdstc2);
+            }
+
+
+            List<DataFilter> filterList = new List<DataFilter>();
+            List<DataFilter> foodList = new List<DataFilter>();
+    
+                foodList.Add(new DataFilter()
+                {
+                    type = "Food",
+                    value = fd2.Id
+
+                });
+                filterList.Add(new DataFilter()
+                {
+                    type = "Food",
+                    field = foodList
+                });
+
+
+                FoodStatistic fdstc = iFoodStatisticRepository.Search(filterList).FirstOrDefault();
+
+                Assert.IsTrue(fdstc.Food.Name == fdName2, string.Format("实际结果：{0}与期望结果{1}不一致", fdstc.Food.Name, fdName2));
+        
+
             
-            //FoodStatistic fs2 = new FoodStatistic()
-            //{
-            //    Food = fd,
-            //    Amount = 8,
-            //    Year = 2013,
-            //    Month = 1,
-            //    Day = 2
-            //};
-            //iFoodStatisticRepository.SaveOrUpdate(fs2);
-
-            //FoodStatistic fs3 = new FoodStatistic()
-            //{
-            //    Food = fd,
-            //    Amount = 7,
-            //    Year = 2013,
-            //    Month = 1,
-            //    Day = 3
-            //};
-            //iFoodStatisticRepository.SaveOrUpdate(fs3);
-
-
-            //IRepository<Rent> iRentRepository = UnityHelper.UnityToT<IRepository<Rent>>();
-            //IRepository<House> iHouseRepository = UnityHelper.UnityToT<IRepository<House>>();
-            //Rent rt = new Rent()
-            //{
-            //    Name = "安泰置业",
-
-            //};
-            //iRentRepository.SaveOrUpdate(rt);
-
-            //MerchantGoodsType mgt = new MerchantGoodsType()
-            //{
-            //    Merchant = rt,
-            //    GoodsType = "学区房"
-            //};
-            //iMerchantGoodsTypeRepository.SaveOrUpdate(mgt);
-
-            //House h = new House()
-            //{
-            //    Name = "山财大学区房",
-            //    Amount = 100,
-            //     Rent = rt,
-            //    MerchantGoodsType = mgt,
-            //    TimeOfRentFrom=DateTime.Now,
-            //     TimeOfRentTO=DateTime.Now
-            //};
-            //iHouseRepository.SaveOrUpdate(h);
 
         }
 
