70e3659c76c792bcbb60d2c2ea057260e5a01407
basilwang <ocam@163.com>
2013-03-06 15:17:11 +0800

    add sth

--- Friday.mvc/apps/tmall/fp/2012/fp.js
+++ Friday.mvc/apps/tmall/fp/2012/fp.js
@@ -1,22 +1,22 @@
 KISSY.add("2012/fp", function(_kissy, _datalazyload, _slide2, _category, _brand, _catefold, _floor, _directpromo, _act) {
-    var A = _kissy.DOM,
-        Q = _kissy.Event,
-        K = _kissy.UA;
-    var J = window,
-        O = document,
-        F = J.g_config;
+    var _dom = _kissy.DOM,
+        _event = _kissy.Event,
+        _ua = _kissy.UA;
+    var _window = window,
+        _document = document,
+        _g_config = _window.g_config;
 
-    function B() {
+    function FP() {
         this._init()
     }
-    _kissy.augment(B, {
+    _kissy.augment(FP, {
         _init: function() {
-            var D = this;
-            D._initMain();
+            var _fp = this;
+            _fp._initMain();
             _kissy.later(this._initExtra, 0, false, this);
             _kissy.ready(function() {
-                !F.closeCate && K.ie !== 6 && !K.mobile && new _catefold();
-                D._loadBackTop();
+                !_g_config.closeCate && _ua.ie !== 6 && !_ua.mobile && new _catefold();
+                _fp._loadBackTop();
                 if (~location.href.indexOf("__tms__")) {
                     _kissy.later(function() {
                         _kissy.use("2012/tms/tms", function(E, T) {
@@ -41,29 +41,29 @@ KISSY.add("2012/fp", function(_kissy, _datalazyload, _slide2, _category, _brand,
         },
         _initExtra: function() {
             this._initHeader();
-            var D = new _datalazyload({
+            var _lazyload = new _datalazyload({
                 mod: "manual",
                 diff: 50
             });
-            !F.closeDirect && _directpromo.init([285, 333, 337, 315]);
+            !_g_config.closeDirect && _directpromo.init([285, 333, 337, 315]);
             new _act();
             this._initQuickSearch();
             MFP.POC.add("extra")
         },
         _loadBackTop: function() {
             var D = function() {
-                if (A.scrollTop(O) > 400) {
+                if (_dom.scrollTop(_document) > 400) {
                     _kissy.log("load backtop ...");
                     _kissy.getScript("http://a.tbcdn.cn/apps/tmall/mui/backtop/js/backtop.js");
-                    Q.detach(J, "scroll", D)
+                    _event.detach(_window, "scroll", D)
                 }
             };
-            Q.on(J, "scroll", D)
+            _event.on(_window, "scroll", D)
         },
         _initHeader: function() {
             var S = _kissy.get("#mallSearch");
             var T = _kissy.get("#mq");
-            var E = A.prev(T);
+            var E = _dom.prev(T);
             var D = function() {
                 setTimeout(function() {
                     if (T.value == "") {
@@ -76,31 +76,31 @@ KISSY.add("2012/fp", function(_kissy, _datalazyload, _slide2, _category, _brand,
                 _kissy.use("2012/mods/header", function(W, X) {
                     X.init()
                 });
-                Q.detach(S, "mouseenter", U);
-                Q.detach(T, "keydown", U)
+                _event.detach(S, "mouseenter", U);
+                _event.detach(T, "keydown", U)
             };
             D();
-            Q.on(S, "mouseenter", U);
-            Q.on(T, "keydown", U)
+            _event.on(S, "mouseenter", U);
+            _event.on(T, "keydown", U)
         },
         _initQuickSearch: function() {
             _kissy.log("init quick search ...");
             var D = function() {
-                var S = arguments[0] || J.event,
+                var S = arguments[0] || _window.event,
                     E = S.keyCode;
                 if (S.shiftKey && (E == 191 || E == 229)) {
-                    _kissy.getScript("http://" + F.assetsServer + "/apps/tmall/header/common/quick-search.css", function() {
-                        _kissy.getScript("http://" + F.assetsServer + "/apps/tmall/header/common/quick-search.js")
+                    _kissy.getScript("http://" + _g_config.assetsServer + "/apps/tmall/header/common/quick-search.css", function() {
+                        _kissy.getScript("http://" + _g_config.assetsServer + "/apps/tmall/header/common/quick-search.js")
                     });
-                    Q.detach(O.body, "keydown", arguments.callee)
+                    _event.detach(_document.body, "keydown", arguments.callee)
                 }
             };
-            Q.on(O.body, "keydown", D)
+            _event.on(_document.body, "keydown", D)
         },
         _bindViewChange: function() {
-            var D = J.g_config.view;
-            Q.on(J, "resize", function() {
-                var E = A.viewportWidth();
+            var D = _window.g_config.view;
+            _event.on(_window, "resize", function() {
+                var E = _dom.viewportWidth();
                 var S = E < 1210 ? 990 : 1190;
                 if (S !== D) {
                     MFP.fire("viewChange", {
@@ -111,14 +111,14 @@ KISSY.add("2012/fp", function(_kissy, _datalazyload, _slide2, _category, _brand,
                     D = S
                 }
             });
-            K.ie < 9 && MFP.on("viewChange", function(S) {
-                var E = O.body;
-                A.removeClass(E, "w" + S.oldView);
-                A.addClass(E, "w" + S.newView)
+            _ua.ie < 9 && MFP.on("viewChange", function(S) {
+                var E = _document.body;
+                _dom.removeClass(E, "w" + S.oldView);
+                _dom.addClass(E, "w" + S.newView)
             })
         }
     });
-    return B
+    return FP
 }, {
     requires: ["datalazyload", "2012/mods/slide2", "2012/mods/category", "2012/mods/brand", "2012/mods/cate-fold", "2012/mods/floor", "2012/mods/direct-promo", "2012/mods/act"]
 }); 
