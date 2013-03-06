2294bbbc609140e1c45a87107132cf1fd2e28741
basilwang <ocam@163.com>
2013-03-06 11:30:04 +0800

    async load sub category

---
+++ Friday.mvc/Areas/Category/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Friday.mvc.Areas.Category.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Category/Home/
+
+        public ActionResult all_cat_asyn()
+        {
+            return View();
+        }
+
+    }
+}
