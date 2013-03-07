42713ea3b87d74da1937c415b7fccaa136a25277
wanghaichuan <haichuan300@163.com>
2013-03-07 09:00:32 +0800

    Add ValuingOfMyHouseOrderRepository.cs

---
+++ Friday.core/repositories/IValuingOfMyHouseOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.components;
+
+namespace friday.core.repositories
+{
+    public interface IValuingOfMyHouseOrderRepository : IRepository<ValuingOfMyHouseOrder>
+    {
+        IList<ValuingOfMyHouseOrder> Search(List<DataFilter> termList);
+        IList<ValuingOfMyHouseOrder> Search(List<DataFilter> termList, int start, int limit, out long total);
+    }
+}
---
+++ Friday.core/repositories/ValuingOfMyHouseOrderRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.components;
+using NHibernate;
+
+namespace friday.core.repositories
+{
+    public class ValuingOfMyHouseOrderRepository : Repository<ValuingOfMyHouseOrder>, IValuingOfMyHouseOrderRepository
+    {
+        protected virtual ICriteria Query
+        {
+            get { return Session.CreateCriteria(typeof(ValuingOfMyHouseOrder)); }
+        }
+        //对外获取方法
+        public IList<ValuingOfMyHouseOrder> Search(List<DataFilter> termList)
+        {
+            return SearchByValuingOfMyHouseOrder(Query, termList).List<ValuingOfMyHouseOrder>();
+        }
+        public IList<ValuingOfMyHouseOrder> Search(List<DataFilter> termList, int start, int limit, out long total)
+        {
+            ICriteria query = SearchByValuingOfMyHouseOrder(Query, termList);
+            //2013-02-11 basilwang must use projection.rowcount and clear order ( sql does't support only count(*) and order by)
+            ICriteria countCriteria = CriteriaTransformer.Clone(query)
+            .SetProjection(NHibernate.Criterion.Projections.RowCountInt64());
+
+            countCriteria.ClearOrders();
+            total = countCriteria.UniqueResult<long>();
+            return query.SetFirstResult(start)
+                 .SetMaxResults(limit)
+                 .List<ValuingOfMyHouseOrder>();
+        }
+    }
+
+}
