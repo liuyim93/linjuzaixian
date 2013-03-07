b2528e6a1c5a2556c597305127f284725be30eb9
pangfuxing <pang_fu_xing@126.com>
2013-03-07 08:10:17 +0800

    Add  Test

---
+++ Friday.Test2/Test_CommodityStatistic.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using friday.core.domain;
+using friday.core.repositories;
+using friday.core;
+using friday.core.EnumType;
+using friday.core.components;
+using System.Linq;
+using System.Transactions;
+
+
+namespace Friday.Test2
+{
+    [TestFixture]
+    public class Test_CommodityStatistic
+    {
+        private TransactionScope scope;
+        [SetUp]
+        public void SetUp()
+        {
+            scope = new TransactionScope();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            scope.Dispose();
+        }
+        [Test]
+        public void Test()
+        {
+            ICommodityStatisticRepository iCommodityStatisticRepository = UnityHelper.UnityToT<ICommodityStatisticRepository>();
+            IRepository<Commodity> iCommodityRepository = UnityHelper.UnityToT<IRepository<Commodity>>();
+            IRepository<Shop> iShopRepository = UnityHelper.UnityToT<IRepository<Shop>>();
+            IRepository<MerchantGoodsType> iMerchantGoodsTypeRepository = UnityHelper.UnityToT<IRepository<MerchantGoodsType>>();
+            IList<CommodityStatistic> iCommodityStatistics = new List<CommodityStatistic>();
+
+            string shpName1 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string shpName2 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string mgName1 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string mgName2 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string cmdName1 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string cmdName2 = Guid.NewGuid().ToString().GetHashCode().ToString();
+
+            Shop shp1 = new Shop()
+            {
+                Name = shpName1,
+                 MerchantType=0
+            };
+            iShopRepository.SaveOrUpdate(shp1);
+
+            MerchantGoodsType mg1 = new MerchantGoodsType()
+            {
+                Merchant = shp1,
+                GoodsType = mgName1
+            };
+            iMerchantGoodsTypeRepository.SaveOrUpdate(mg1);
+
+
+
+            Shop shp2 = new Shop()
+            {
+                Name = shpName2,
+                MerchantType = 0
+            };
+            iShopRepository.SaveOrUpdate(shp2);
+
+            MerchantGoodsType mg2 = new MerchantGoodsType()
+            {
+                Merchant = shp2,
+                GoodsType = mgName2
+            };
+            iMerchantGoodsTypeRepository.SaveOrUpdate(mg2);
+
+            Commodity cmd1 = new Commodity()
+            {
+                Name = cmdName1,
+                Amount = 100,
+                Shop = shp1,
+                MerchantGoodsType = mg1
+            };
+            iCommodityRepository.SaveOrUpdate(cmd1);
+
+            Commodity cmd2 = new Commodity()
+            {
+                Name = cmdName2,
+                Amount = 100,
+                Shop = shp2,
+                MerchantGoodsType = mg2
+            };
+            iCommodityRepository.SaveOrUpdate(cmd2);
+
+            for (int i = 0; i < 10; i++) 
+            {
+
+                CommodityStatistic fdstc1 = new CommodityStatistic()
+                {
+                    Year = 2013,
+                    Month = 3,
+                    Day = i,
+                    AverageValuing = i,
+                    ValuingCount = i,
+                    Amount = i,
+                    Commodity = cmd1
+                };
+                iCommodityStatisticRepository.SaveOrUpdate(fdstc1);
+
+                CommodityStatistic fdstc2 = new CommodityStatistic()
+                {
+                    Year = 2013,
+                    Month = 3,
+                    Day = i,
+                    AverageValuing = i,
+                    ValuingCount = i,
+                    Amount = i,
+                    Commodity = cmd2
+                };
+                iCommodityStatisticRepository.SaveOrUpdate(fdstc2);
+            }
+
+
+            List<DataFilter> filterList = new List<DataFilter>();
+            List<DataFilter> commodityList = new List<DataFilter>();
+    
+                commodityList.Add(new DataFilter()
+                {
+                    type = "Commodity",
+                    value = cmd2.Id
+
+                });
+                filterList.Add(new DataFilter()
+                {
+                    type = "Commodity",
+                    field = commodityList
+                });
+
+
+                CommodityStatistic fdstc = iCommodityStatisticRepository.Search(filterList).FirstOrDefault();
+
+                Assert.IsTrue(fdstc.Commodity.Name == cmdName2, string.Format("实际结果：{0}与期望结果{1}不一致", fdstc.Commodity.Name, cmdName2));
+        
+
+            
+
+        }
+
+     
+    }
+}
---
+++ Friday.Test2/Test_HouseStatistic.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using friday.core.domain;
+using friday.core.repositories;
+using friday.core;
+using friday.core.EnumType;
+using friday.core.components;
+using System.Linq;
+using System.Transactions;
+
+
+namespace Friday.Test2
+{
+    [TestFixture]
+    public class Test_HouseStatistic
+    {
+        private TransactionScope scope;
+        [SetUp]
+        public void SetUp()
+        {
+            scope = new TransactionScope();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            scope.Dispose();
+        }
+        [Test]
+        public void Test()
+        {
+            IHouseStatisticRepository iHouseStatisticRepository = UnityHelper.UnityToT<IHouseStatisticRepository>();
+            IRepository<House> iHouseRepository = UnityHelper.UnityToT<IRepository<House>>();
+            IRepository<Rent> iRentRepository = UnityHelper.UnityToT<IRepository<Rent>>();
+            IRepository<MerchantGoodsType> iMerchantGoodsTypeRepository = UnityHelper.UnityToT<IRepository<MerchantGoodsType>>();
+            IList<HouseStatistic> iHouseStatistics = new List<HouseStatistic>();
+
+            string rtName1 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string rtName2 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string mgName1 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string mgName2 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string huName1 = Guid.NewGuid().ToString().GetHashCode().ToString();
+            string huName2 = Guid.NewGuid().ToString().GetHashCode().ToString();
+
+            Rent rst1 = new Rent()
+            {
+                Name = rtName1,
+                 MerchantType=0
+            };
+            iRentRepository.SaveOrUpdate(rst1);
+
+            MerchantGoodsType mg1 = new MerchantGoodsType()
+            {
+                Merchant = rst1,
+                GoodsType = mgName1
+            };
+            iMerchantGoodsTypeRepository.SaveOrUpdate(mg1);
+
+
+
+            Rent rst2 = new Rent()
+            {
+                Name = rtName2,
+                MerchantType = 0
+            };
+            iRentRepository.SaveOrUpdate(rst2);
+
+            MerchantGoodsType mg2 = new MerchantGoodsType()
+            {
+                Merchant = rst2,
+                GoodsType = mgName2
+            };
+            iMerchantGoodsTypeRepository.SaveOrUpdate(mg2);
+
+            House hu1 = new House()
+            {
+                Name = huName1,
+                Amount = 100,
+                Rent = rst1,
+                MerchantGoodsType = mg1,
+                 TimeOfRentFrom=DateTime.Now,
+                  TimeOfRentTO=DateTime.Now
+            };
+            iHouseRepository.SaveOrUpdate(hu1);
+
+            House hu2 = new House()
+            {
+                Name = huName2,
+                Amount = 100,
+                Rent = rst2,
+                MerchantGoodsType = mg2,
+                TimeOfRentFrom = DateTime.Now,
+                TimeOfRentTO = DateTime.Now
+            };
+            iHouseRepository.SaveOrUpdate(hu2);
+
+            for (int i = 0; i < 10; i++) 
+            {
+
+                HouseStatistic fdstc1 = new HouseStatistic()
+                {
+                    Year = 2013,
+                    Month = 3,
+                    Day = i,
+                    AverageValuing = i,
+                    ValuingCount = i,
+                    Amount = i,
+                    House = hu1
+                };
+                iHouseStatisticRepository.SaveOrUpdate(fdstc1);
+
+                HouseStatistic fdstc2 = new HouseStatistic()
+                {
+                    Year = 2013,
+                    Month = 3,
+                    Day = i,
+                    AverageValuing = i,
+                    ValuingCount = i,
+                    Amount = i,
+                    House = hu2
+                };
+                iHouseStatisticRepository.SaveOrUpdate(fdstc2);
+            }
+
+
+            List<DataFilter> filterList = new List<DataFilter>();
+            List<DataFilter> houseList = new List<DataFilter>();
+    
+                houseList.Add(new DataFilter()
+                {
+                    type = "House",
+                    value = hu2.Id
+
+                });
+                filterList.Add(new DataFilter()
+                {
+                    type = "House",
+                    field = houseList
+                });
+
+
+                HouseStatistic fdstc = iHouseStatisticRepository.Search(filterList).FirstOrDefault();
+
+                Assert.IsTrue(fdstc.House.Name == huName2, string.Format("实际结果：{0}与期望结果{1}不一致", fdstc.House.Name, huName2));
+        
+
+            
+
+        }
+
+     
+    }
+}
