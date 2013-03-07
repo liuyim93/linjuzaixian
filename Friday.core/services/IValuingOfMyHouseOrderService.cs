7093a1c8f39f95dbb6abfeab8cb2cad14cd9f402
wanghaichuan <haichuan300@163.com>
2013-03-07 09:56:54 +0800

    Add IValuingOfMyHouseOrderService.cs

---
+++ Friday.core/services/IValuingOfMyHouseOrderService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public interface IValuingOfMyHouseOrderService
+    {
+        ValuingOfMyHouseOrder Load(string id);
+        void Save(ValuingOfMyHouseOrder valuingOfMyHouseOrder);
+        void Update(ValuingOfMyHouseOrder valuingOfMyHouseOrder);
+        void Delete(string id);
+        IList<ValuingOfMyHouseOrder> Search(List<DataFilter> termList);
+        IList<ValuingOfMyHouseOrder> Search(List<DataFilter> termList, int start, int limit, out long total);
+    }
+}
---
+++ Friday.core/services/ValuingOfMyHouseOrderService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.repositories;
+using friday.core.utils;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public class ValuingOfMyHouseOrderService : IValuingOfMyHouseOrderService
+    {
+        private IValuingOfMyHouseOrderRepository iValuingOfMyHouseOrderRepository;
+        private ILogger iLogger;
+        public ValuingOfMyHouseOrderService(IValuingOfMyHouseOrderRepository iValuingOfMyHouseOrderRepository, ILogger iLogger)
+        {
+            this.iValuingOfMyHouseOrderRepository = iValuingOfMyHouseOrderRepository;
+            this.iLogger = iLogger;
+        }
+        public ValuingOfMyHouseOrder Load(string id)
+        {
+            return iValuingOfMyHouseOrderRepository.Load(id);
+        }
+
+        public void Save(ValuingOfMyHouseOrder valuingOfMyHouseOrder)
+        {
+            iLogger.LogMessage("插入ValuingOfMyHouseOrder数据，ID：" + valuingOfMyHouseOrder.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iValuingOfMyHouseOrderRepository.SaveOrUpdate(valuingOfMyHouseOrder);
+        }
+
+        public void Update(ValuingOfMyHouseOrder valuingOfMyHouseOrder)
+        {
+            iLogger.LogMessage("更新ValuingOfMyHouseOrder数据，ID：" + valuingOfMyHouseOrder.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iValuingOfMyHouseOrderRepository.SaveOrUpdate(valuingOfMyHouseOrder);
+        }
+
+        public void Delete(string id)
+        {
+            iLogger.LogMessage("删除ValuingOfMyHouseOrder数据，ID：" + id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iValuingOfMyHouseOrderRepository.Delete(id);
+        }
+
+
+        public IList<ValuingOfMyHouseOrder> Search(List<DataFilter> termList)
+        {
+            return iValuingOfMyHouseOrderRepository.Search(termList);
+        }
+
+        public IList<ValuingOfMyHouseOrder> Search(List<DataFilter> termList, int start, int limit, out long total)
+        {
+            return iValuingOfMyHouseOrderRepository.Search(termList, start, limit, out total);
+        }
+    }
+}
