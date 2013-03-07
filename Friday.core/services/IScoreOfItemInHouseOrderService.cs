d5ada49eedc015e0241c54dac466876670a2ff33
wanghaichuan <haichuan300@163.com>
2013-03-07 09:48:49 +0800

    Add ScoreOfItemInHouseOrderService.cs

---
+++ Friday.core/services/IScoreOfItemInHouseOrderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.domain;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public interface IScoreOfItemInHouseOrderService
+    {
+        ScoreOfItemInHouseOrder Load(string id);
+        void Save(ScoreOfItemInHouseOrder scoreOfItemInHouseOrder);
+        void Update(ScoreOfItemInHouseOrder scoreOfItemInHouseOrder);
+        void Delete(string id);
+        IList<ScoreOfItemInHouseOrder> Search(List<DataFilter> termList);
+        IList<ScoreOfItemInHouseOrder> Search(List<DataFilter> termList, int start, int limit, out long total);
+    }
+}
---
+++ Friday.core/services/ScoreOfItemInHouseOrderService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.utils;
+using friday.core.repositories;
+using friday.core.components;
+using friday.core.domain;
+
+namespace friday.core.services
+{
+    public class ScoreOfItemInHouseOrderService : IScoreOfItemInHouseOrderService
+    {
+        private IScoreOfItemInHouseOrderRepository iScoreOfItemInHouseOrderRepository;
+        private ILogger iLogger;
+        public ScoreOfItemInHouseOrderService(IScoreOfItemInHouseOrderRepository iScoreOfItemInHouseOrderRepository, ILogger iLogger)
+        {
+            this.iScoreOfItemInHouseOrderRepository = iScoreOfItemInHouseOrderRepository;
+            this.iLogger = iLogger;
+        }
+        public ScoreOfItemInHouseOrder Load(string id)
+        {
+            return iScoreOfItemInHouseOrderRepository.Load(id);
+        }
+
+        public void Save(ScoreOfItemInHouseOrder scoreOfItemInHouseOrder)
+        {
+            iLogger.LogMessage("插入ScoreOfItemInHouseOrder数据，ID：" + scoreOfItemInHouseOrder.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iScoreOfItemInHouseOrderRepository.SaveOrUpdate(scoreOfItemInHouseOrder);
+        }
+
+        public void Update(ScoreOfItemInHouseOrder scoreOfItemInHouseOrder)
+        {
+            iLogger.LogMessage("更新ScoreOfItemInHouseOrder数据，ID：" + scoreOfItemInHouseOrder.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iScoreOfItemInHouseOrderRepository.SaveOrUpdate(scoreOfItemInHouseOrder);
+        }
+
+        public void Delete(string id)
+        {
+            iLogger.LogMessage("删除ScoreOfItemInHouseOrder数据，ID：" + id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iScoreOfItemInHouseOrderRepository.Delete(id);
+        }
+
+
+        public IList<ScoreOfItemInHouseOrder> Search(List<DataFilter> termList)
+        {
+            return iScoreOfItemInHouseOrderRepository.Search(termList);
+        }
+
+        public IList<ScoreOfItemInHouseOrder> Search(List<DataFilter> termList, int start, int limit, out long total)
+        {
+            return iScoreOfItemInHouseOrderRepository.Search(termList, start, limit, out total);
+        }
+    }
+}
