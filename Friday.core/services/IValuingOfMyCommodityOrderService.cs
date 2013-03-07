2b0c3c54ec63bc770cc0ac802bc09d3a9d5b6469
wanghaichuan <haichuan300@163.com>
2013-03-07 09:52:52 +0800

    Add ValuingOfMyCommodityOrderService.cs

---
+++ Friday.core/services/IValuingOfMyCommodityOrderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.domain;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public interface IValuingOfMyCommodityOrderService
+    {
+        ValuingOfMyCommodityOrder Load(string id);
+        void Save(ValuingOfMyCommodityOrder valuingOfMyCommodityOrder);
+        void Update(ValuingOfMyCommodityOrder valuingOfMyCommodityOrder);
+        void Delete(string id);
+        IList<ValuingOfMyCommodityOrder> Search(List<DataFilter> termList);
+        IList<ValuingOfMyCommodityOrder> Search(List<DataFilter> termList, int start, int limit, out long total);
+    }
+}
---
+++ Friday.core/services/ValuingOfMyCommodityOrderService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.repositories;
+using friday.core.utils;
+using friday.core.domain;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public class ValuingOfMyCommodityOrderService : IValuingOfMyCommodityOrderService
+    {
+        private IValuingOfMyCommodityOrderRepository iValuingOfMyCommodityOrderRepository;
+        private ILogger iLogger;
+        public ValuingOfMyCommodityOrderService(IValuingOfMyCommodityOrderRepository iValuingOfMyCommodityOrderRepository, ILogger iLogger)
+        {
+            this.iValuingOfMyCommodityOrderRepository = iValuingOfMyCommodityOrderRepository;
+            this.iLogger = iLogger;
+        }
+        public ValuingOfMyCommodityOrder Load(string id)
+        {
+            return iValuingOfMyCommodityOrderRepository.Load(id);
+        }
+
+        public void Save(ValuingOfMyCommodityOrder valuingOfMyCommodityOrder)
+        {
+            iLogger.LogMessage("插入ValuingOfMyCommodityOrder数据，ID：" + valuingOfMyCommodityOrder.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iValuingOfMyCommodityOrderRepository.SaveOrUpdate(valuingOfMyCommodityOrder);
+        }
+
+        public void Update(ValuingOfMyCommodityOrder valuingOfMyCommodityOrder)
+        {
+            iLogger.LogMessage("更新ValuingOfMyCommodityOrder数据，ID：" + valuingOfMyCommodityOrder.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iValuingOfMyCommodityOrderRepository.SaveOrUpdate(valuingOfMyCommodityOrder);
+        }
+
+        public void Delete(string id)
+        {
+            iLogger.LogMessage("删除ValuingOfMyCommodityOrder数据，ID：" + id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iValuingOfMyCommodityOrderRepository.Delete(id);
+        }
+
+
+        public IList<ValuingOfMyCommodityOrder> Search(List<DataFilter> termList)
+        {
+            return iValuingOfMyCommodityOrderRepository.Search(termList);
+        }
+
+        public IList<ValuingOfMyCommodityOrder> Search(List<DataFilter> termList, int start, int limit, out long total)
+        {
+            return iValuingOfMyCommodityOrderRepository.Search(termList, start, limit, out total);
+        }
+    }
+}
