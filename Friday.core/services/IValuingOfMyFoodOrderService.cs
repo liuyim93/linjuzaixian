dc158a4873b5cda2c4282951e0ac6d31e9ca570b
wanghaichuan <haichuan300@163.com>
2013-03-07 09:54:50 +0800

    Add ValuingOfMyFoodOrderService.cs

---
+++ Friday.core/services/IValuingOfMyFoodOrderService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public interface IValuingOfMyFoodOrderService
+    {
+        ValuingOfMyFoodOrder Load(string id);
+        void Save(ValuingOfMyFoodOrder valuingOfMyFoodOrder);
+        void Update(ValuingOfMyFoodOrder valuingOfMyFoodOrder);
+        void Delete(string id);
+        IList<ValuingOfMyFoodOrder> Search(List<DataFilter> termList);
+        IList<ValuingOfMyFoodOrder> Search(List<DataFilter> termList, int start, int limit, out long total);
+    }
+}
---
+++ Friday.core/services/ValuingOfMyFoodOrderService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.repositories;
+using friday.core.utils;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public class ValuingOfMyFoodOrderService : IValuingOfMyFoodOrderService
+    {
+        private IValuingOfMyFoodOrderRepository iValuingOfMyFoodOrderRepository;
+        private ILogger iLogger;
+        public ValuingOfMyFoodOrderService(IValuingOfMyFoodOrderRepository iValuingOfMyFoodOrderRepository, ILogger iLogger)
+        {
+            this.iValuingOfMyFoodOrderRepository = iValuingOfMyFoodOrderRepository;
+            this.iLogger = iLogger;
+        }
+        public ValuingOfMyFoodOrder Load(string id)
+        {
+            return iValuingOfMyFoodOrderRepository.Load(id);
+        }
+
+        public void Save(ValuingOfMyFoodOrder valuingOfMyFoodOrder)
+        {
+            iLogger.LogMessage("插入ValuingOfMyFoodOrder数据，ID：" + valuingOfMyFoodOrder.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iValuingOfMyFoodOrderRepository.SaveOrUpdate(valuingOfMyFoodOrder);
+        }
+
+        public void Update(ValuingOfMyFoodOrder valuingOfMyFoodOrder)
+        {
+            iLogger.LogMessage("更新ValuingOfMyFoodOrder数据，ID：" + valuingOfMyFoodOrder.Id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iValuingOfMyFoodOrderRepository.SaveOrUpdate(valuingOfMyFoodOrder);
+        }
+
+        public void Delete(string id)
+        {
+            iLogger.LogMessage("删除ValuingOfMyFoodOrder数据，ID：" + id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iValuingOfMyFoodOrderRepository.Delete(id);
+        }
+
+
+        public IList<ValuingOfMyFoodOrder> Search(List<DataFilter> termList)
+        {
+            return iValuingOfMyFoodOrderRepository.Search(termList);
+        }
+
+        public IList<ValuingOfMyFoodOrder> Search(List<DataFilter> termList, int start, int limit, out long total)
+        {
+            return iValuingOfMyFoodOrderRepository.Search(termList, start, limit, out total);
+        }
+    }
+}
