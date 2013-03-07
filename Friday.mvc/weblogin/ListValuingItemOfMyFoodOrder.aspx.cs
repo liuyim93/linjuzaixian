6c0a2352e3c6bb59a91f823f38acae864e9dfd9d
wanghaichuan <haichuan300@163.com>
2013-03-07 10:38:32 +0800

    Add ListValuingItemOfMyFoodOrder.aspx

---
+++ Friday.mvc/weblogin/ListValuingItemOfMyFoodOrder.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using friday.core.repositories;
+using friday.core;
+
+namespace Friday.mvc.weblogin
+{
+    public partial class ListValuingItemOfMyFoodOrder : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IRepository<ValuingItemOfMyFoodOrder> repoValuingItemOfMyFoodOrder = UnityHelper.UnityToT<IRepository<ValuingItemOfMyFoodOrder>>();
+            IList<ValuingItemOfMyFoodOrder> valuingItemOfMyFoodOrders = new List<ValuingItemOfMyFoodOrder>();
+            valuingItemOfMyFoodOrders = repoValuingItemOfMyFoodOrder.GetAll();
+            repeater.DataSource = valuingItemOfMyFoodOrders;
+            repeater.DataBind();
+        }
+    }
+}
