539002fe1cc00763db5df21c66e728dd643dc4ba
wanghaichuan <haichuan300@163.com>
2013-03-11 15:11:56 +0800

    hide ValuingComments menu

--- Friday.Test2/PopulateRealPermissionDataTest.cs
+++ Friday.Test2/PopulateRealPermissionDataTest.cs
@@ -363,17 +363,17 @@ namespace Friday.Test2
             iSystemMenuRepository.SaveOrUpdate(school);
             adminMenuCheckList.Add(school);
 
-            SystemMenu valuingComments = new SystemMenu()
-            {
-                Name = "评论回复管理",
-                Leaf = true,
-                ParentID = baseInfo.Id,
-                MenuRoute = "valuingComments/pValuingCommentsList.aspx",
-                TLevel = 1,
-                ColIndex = 11
-            };
-            iSystemMenuRepository.SaveOrUpdate(valuingComments);
-            adminMenuCheckList.Add(valuingComments);
+            //SystemMenu valuingComments = new SystemMenu()
+            //{
+            //    Name = "评论回复管理",
+            //    Leaf = true,
+            //    ParentID = baseInfo.Id,
+            //    MenuRoute = "valuingComments/pValuingCommentsList.aspx",
+            //    TLevel = 1,
+            //    ColIndex = 11
+            //};
+            //iSystemMenuRepository.SaveOrUpdate(valuingComments);
+            //adminMenuCheckList.Add(valuingComments);
 
             //消息模块
             SystemMenu messageModel = new SystemMenu()
