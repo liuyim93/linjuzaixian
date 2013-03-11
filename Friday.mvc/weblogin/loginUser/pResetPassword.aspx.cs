64c60e879caf320a76aa1fc1de34c6ee9a23bf0e
pangfuxing <pang_fu_xing@126.com>
2013-03-11 18:19:15 +0800

    Add  Tag In LoginUser

--- Friday.mvc/weblogin/loginUser/pResetPassword.aspx.cs
+++ Friday.mvc/weblogin/loginUser/pResetPassword.aspx.cs
@@ -8,17 +8,18 @@ using friday.core.repositories;
 using friday.core;
 using friday.core.components;
 using friday.core.domain;
+using friday.core.services;
 
 namespace Friday.mvc.weblogin
 {
     public partial class pResetPassword : BasePage
     {
-        IRepository<LoginUser> iLoginUserRepository = UnityHelper.UnityToT<IRepository<LoginUser>>();
+        ILoginUserService iLoginUserService = UnityHelper.UnityToT<ILoginUserService>();
         private LoginUser loginUser;
         protected void Page_Load(object sender, EventArgs e)
         {
             string uid = Request.Params["uid"].ToString();
-            loginUser = iLoginUserRepository.Load(uid);
+            loginUser = iLoginUserService.Load(uid);
             if (Request.Params["__EVENTVALIDATION"] != null)
             {
 
@@ -31,7 +32,7 @@ namespace Friday.mvc.weblogin
         {
 
             BindingHelper.RequestToObject(loginUser);
-            iLoginUserRepository.SaveOrUpdate(loginUser);
+            iLoginUserService.Update(loginUser);
 
             AjaxResult result = new AjaxResult();
             result.statusCode = "200";
