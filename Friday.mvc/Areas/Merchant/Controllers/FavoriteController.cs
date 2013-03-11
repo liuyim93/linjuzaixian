154bd29d71b696c07fbb0805125002855b4a1240
wanghaichuan <haichuan300@163.com>
2013-03-11 15:35:12 +0800

    Add saveMyFavorite function

--- Friday.mvc/Areas/Merchant/Controllers/FavoriteController.cs
+++ Friday.mvc/Areas/Merchant/Controllers/FavoriteController.cs
@@ -4,15 +4,21 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using friday.core.services;
+using friday.core.domain;
 
 namespace Friday.mvc.Areas.Merchant.Controllers
 {
     public class FavoriteController : Controller
     {  
         private IMerchantService iMerchantService;
-        public FavoriteController(IMerchantService iMerchantService)
+        private IUserService iUserService;
+        private IMyFavoriteService iMyFavoriteService;
+
+        public FavoriteController(IMerchantService iMerchantService, IUserService iUserService, IMyFavoriteService iMyFavoriteService)
         {
             this.iMerchantService = iMerchantService;
+            this.iUserService = iUserService;
+            this.iMyFavoriteService = iMyFavoriteService;
         }
         public ActionResult Index()
         {
@@ -20,6 +26,13 @@ namespace Friday.mvc.Areas.Merchant.Controllers
         }
         public ActionResult AddToFav(string brandId,string _tb_token_,string callback)
         {
+            MyFavorite myFavorite = new MyFavorite();
+            SystemUser systemUser = iUserService.GetOrCreateUser(this.HttpContext);
+            friday.core.Merchant merchant = iMerchantService.Load(brandId);
+            myFavorite.SystemUser = systemUser;
+            myFavorite.Merchant = merchant;
+            iMyFavoriteService.Save(myFavorite);
+
             string isSucceed = "T";
             string script = callback + "({\"is_success\":\"" + isSucceed + "\"})";
 
