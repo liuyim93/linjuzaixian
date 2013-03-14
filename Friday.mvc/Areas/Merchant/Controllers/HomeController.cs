05f18904cdda6a26f437458a3e3986e20c0700df
basilwang <ocam@163.com>
2013-03-14 21:43:03 +0800

    add css

---
+++ Friday.mvc/Areas/Merchant/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Friday.mvc.Areas.Merchant.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Merchant/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
