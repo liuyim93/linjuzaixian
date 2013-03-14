dd9067b8a61d16ecb79b9ed33da6882da0aec902
basilwang <ocam@163.com>
2013-03-14 20:00:00 +0800

    add search page

---
+++ Friday.mvc/Areas/Merchant/Controllers/SearchController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Friday.mvc.Areas.Merchant.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Merchant/Search/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
