6f789cac3e47b8f2d5118d509b2f88f9479d974b
pangfuxing <pang_fu_xing@126.com>
2013-04-14 02:22:24 +0800

    Add  SkuProp  repository  and  service   null

---
+++ Friday.core/repositories/ISkuPropRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.domain;
+using friday.core.components;
+
+namespace friday.core.repositories
+{
+    public interface ISkuPropRepository : IRepository<SkuProp>
+    {
+        
+    }
+}
