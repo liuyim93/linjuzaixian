4fd43320a2d7a5bcd8a5ae456199db4eb7acb9c9
wanghaichuan <haichuan300@163.com>
2013-04-14 12:09:24 +0800

    Add SKURepository

---
+++ Friday.core/repositories/ISKURepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.domain;
+
+namespace friday.core.repositories
+{
+    public interface ISKURepository : IRepository<SKU>
+    {
+
+    }
+}
