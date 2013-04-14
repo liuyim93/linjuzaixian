2783cbf9dcc095ece9b8916000ce0c1de5cc691c
wanghaichuan <haichuan300@163.com>
2013-04-14 12:14:50 +0800

    Add SKUService

---
+++ Friday.core/services/ISKUService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.domain;
+using friday.core.components;
+
+namespace friday.core.services
+{
+    public interface ISKUService
+    {
+        SKU Load(string id);
+        void Save(SKU sku);
+        void Update(SKU sku);
+        void Delete(string id);
+    }
+}
---
+++ Friday.core/services/SKUService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.repositories;
+using friday.core.utils;
+using friday.core.domain;
+
+namespace friday.core.services
+{
+    public class SKUService:ISKUService
+    {
+        private ISKURepository iSKURepository;
+        private ILogger iLogger;
+
+        public SKUService(ISKURepository iSKURepository, ILogger iLogger)
+        {
+            this.iSKURepository = iSKURepository;
+            this.iLogger = iLogger;
+        }
+
+        public SKU Load(string id)
+        {
+            return iSKURepository.Load(id);
+        }
+
+        public void Save(SKU sku)
+        {
+            iLogger.LogMessage("插入SKU数据，ID：" + sku.skuId, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iSKURepository.SaveOrUpdate(sku);
+        }
+
+        public void Update(SKU sku)
+        {
+            iLogger.LogMessage("更新SKU数据，ID：" + sku.skuId, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iSKURepository.SaveOrUpdate(sku);
+        }
+
+        public void Delete(string id)
+        {
+            iLogger.LogMessage("删除SKU数据，ID：" + id, this.GetType().FullName, EventDataTypeCategory.操作日志);
+            iSKURepository.Delete(id);
+        }
+    }
+}
