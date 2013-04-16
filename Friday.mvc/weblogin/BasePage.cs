47af2126201015ec4af4ff49a64522658612b26d
pangfuxing <pang_fu_xing@126.com>
2013-04-16 21:25:36 +0800

    fix  BasePage   result.callbackType = "closeCurrent";

--- Friday.mvc/weblogin/BasePage.cs
+++ Friday.mvc/weblogin/BasePage.cs
@@ -39,6 +39,7 @@ namespace Friday.mvc
                  AjaxResult result = new AjaxResult();
                 result.statusCode = "300";
                 result.message = "没有"+tagName+"权限";
+                result.callbackType = "closeCurrent";   
                 FormatJsonResult jsonResult = new FormatJsonResult();
                 jsonResult.Data = result;
                 Response.Write(jsonResult.FormatResult());
