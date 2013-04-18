94c134599477c8a8d8fab4ad3703e9459cd19f32
basilwang <ocam@163.com>
2013-04-18 16:34:38 +0800

    add ishare.  mainbody is ok

---
+++ Friday.mvc/apps/malldetail/4.0/other/ishare.js
@@ -0,0 +1,14 @@
+﻿KISSY.add("malldetail/other/ishare", function (C, B, A) {
+    return { init: function () {
+        B.on("#J_IShare", "click", function (D) {
+            D.preventDefault();
+            C.use("tml/share", function (H, E) {
+                var F = window.g_config;
+                var G = { style: "none", id: 1, container: "#J_IShare", type: "item", showRecTip: false, key: F.itemId };
+                new E(G).popup();
+                H.sendAtpanel("tmalldetail.13.11")
+            })
+        })
+    } 
+    }
+}, { requires: ["event", "malldetail/common/util"] }); /*pub-1|2013-04-11 17:02:57*/
