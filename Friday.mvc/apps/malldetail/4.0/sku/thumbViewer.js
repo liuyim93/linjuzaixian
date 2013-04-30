d48a2dedefd0011da6be20ca7cf32b88eb5102c5
basilwang <ocam@163.com>
2013-05-01 07:28:46 +0800

    rename thumbViewer

--- Friday.mvc/apps/malldetail/4.0/sku/thumbViewer.js
+++ Friday.mvc/apps/malldetail/4.0/sku/thumbViewer.js
@@ -3,41 +3,41 @@
         _dom_id_J_ImgBooth,
         _dom_id_J_UlThumb,
         _dom_li_array,
-        K,
+        _later_fn,
         L = 100,
         _str_classname_tb_selected = "tb-selected",
-        N = "hidden",
+        _STR_HIDDEN = "hidden",
         _zoomlevel,
         _size_config = (window.g_config.D950) ? { size: [460, 460], smallSize: [60, 60], bigSize: [477, 335]} : { size: [310, 310], smallSize: [40, 40], bigSize: [430, 310] };
     var _loader_fn_factory = _malldetail_common_util.createLoader(function (_filter_pipeline_dry_fn) {
         if ("ontouchstart" in document) {
             return
         }
-        var U = { imageNode: _dom_id_J_ImgBooth, width: _size_config.bigSize[0], height: _size_config.bigSize[1], bigImageWidth: 800, bigImageHeight: 800, align: { node: _dom.parent(_dom_id_J_ImgBooth), points: ["tr", "tl"], offset: [10, -1] }, hasZoom: !("ontouchstart" in document), preload: false, showIcon: true };
-        var S = new _imagezoom(U);
-        if (S.lensIcon) {
-            _event.on(S.lensIcon, "mouseenter", function () {
+        var _imagezoom_cfg = { imageNode: _dom_id_J_ImgBooth, width: _size_config.bigSize[0], height: _size_config.bigSize[1], bigImageWidth: 800, bigImageHeight: 800, align: { node: _dom.parent(_dom_id_J_ImgBooth), points: ["tr", "tl"], offset: [10, -1] }, hasZoom: !("ontouchstart" in document), preload: false, showIcon: true };
+        var _imagezoom = new _imagezoom(_imagezoom_cfg);
+        if (_imagezoom.lensIcon) {
+            _event.on(_imagezoom.lensIcon, "mouseenter", function () {
                 _dom.hide(this)
             })
         }
-        _filter_pipeline_dry_fn(S)
+        _filter_pipeline_dry_fn(_imagezoom)
     });
-    function C(U, W, V) {
+    function C(_url, W, V) {
         var S = C.images || (C.images = {});
-        S[U] = S[U] || (S[U] = _malldetail_common_util.createLoader(function (Z) {
-            var X = new Image();
-            _event.on(X, "load", function () {
-                Z(X)
+        S[_url] = S[_url] || (S[_url] = _malldetail_common_util.createLoader(function (_imagezoom_t) {
+            var _images = new Image();
+            _event.on(_images, "load", function () {
+                _imagezoom_t(_images)
             });
-            X.src = U;
+            _images.src = _url;
             var Y = C.elem;
             if (!Y) {
                 Y = C.elem = _dom.create('<div style="width:0;height:0;overflow:hidden;">');
                 _body.insertBefore(Y, _body.firstChild)
             }
-            Y.appendChild(X)
+            Y.appendChild(_images)
         }));
-        return S[U](W, V)
+        return S[_url](W, V)
     }
     function _remove_img_size_to_url(_img) {
         var _url = _img.src || _dom.attr(_img, "data-ks-lazyload");
@@ -58,19 +58,19 @@
             }
         })
     }, _bindEvents: function () {
-        var S = this;
-        _event.on(_dom_id_J_UlThumb, "click", function (U) {
-            U.preventDefault()
+        var _thumbviewer = this;
+        _event.on(_dom_id_J_UlThumb, "click", function (_e) {
+            _e.preventDefault()
         });
-        _event.on(_dom_li_array, "mouseenter", function (U) {
-            K && K.cancel();
-            K = _kissy.later(function () {
-                S._switchTo(U.target);
+        _event.on(_dom_li_array, "mouseenter", function (_e) {
+            _later_fn && _later_fn.cancel();
+            _later_fn = _kissy.later(function () {
+                _thumbviewer._switchTo(_e.target);
                 _kissy.sendAtpanel("tmalldetail.13.6")
             }, L)
         });
         _event.on(_dom_li_array, "mouseleave", function () {
-            K && K.cancel()
+            _later_fn && _later_fn.cancel()
         })
     }, _switchTo: function (_dom_li) {
         if (!_dom_li) {
@@ -86,14 +86,14 @@
         }
         var _url = _url_to_be_added_img_size + "_" + _size_config.size.join("x") + ".jpg";
         var _data_size = _dom.attr(_dom_li, "data-size");
-        var S = function (_data_size_attr) {
+        var _set_zm = function (_data_size_attr) {
             if (_data_size_attr.zm) {
-                _loader_fn_factory(function (a) {
-                    a.set(_url);
-                    a.set("hasZoom", true);
-                    a.set("bigImageSrc", _url_to_be_added_img_size);
-                    a.set("bigImageWidth", _data_size_attr.w);
-                    a.set("bigImageHeight", _data_size_attr.h)
+                _loader_fn_factory(function (_imagezoom_t) {
+                    _imagezoom_t.set(_url);
+                    _imagezoom_t.set("hasZoom", true);
+                    _imagezoom_t.set("bigImageSrc", _url_to_be_added_img_size);
+                    _imagezoom_t.set("bigImageWidth", _data_size_attr.w);
+                    _imagezoom_t.set("bigImageHeight", _data_size_attr.h)
                 })
             }
         };
@@ -101,16 +101,15 @@
         _dom.addClass(_dom_li, _str_classname_tb_selected);
         _data_size = _json.parse(_data_size);
         if (_data_size) {
-            S(_data_size)
+            _set_zm(_data_size)
         } else {
             C(_url_to_be_added_img_size, function (a) {
-                var Z = { w: a.width, h: a.height };
                 Z.zm = (a.width >= _zoomlevel) ? 1 : 0;
                 _dom.attr(_dom_li, { "data-size": _json.stringify(Z) });
-                S(Z)
+                _set_zm(Z)
             })
         }
-    }, show: function (S) {
+    }, show: function (_url) {
         if (!_dom_id_J_ImgBooth || !_dom_id_J_UlThumb) {
             _dom_id_J_ImgBooth = _dom_id_J_ImgBooth || _dom.get("#J_ImgBooth");
             _dom_id_J_UlThumb = _dom_id_J_UlThumb || _dom.get("#J_UlThumb")
@@ -119,9 +118,9 @@
             return
         }
         _dom.removeClass(_dom_li_array, _str_classname_tb_selected);
-        _dom_id_J_ImgBooth.src = S;
-        _loader_fn_factory(function (U) {
-            U && U.set("hasZoom", false)
+        _dom_id_J_ImgBooth.src = _url;
+        _loader_fn_factory(function (_imagezoom_t) {
+            _imagezoom_t && _imagezoom_t.set("hasZoom", false)
         })
     } 
     }
