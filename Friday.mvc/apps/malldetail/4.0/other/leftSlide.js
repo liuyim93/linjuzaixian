11dbb3a8ff591a7d8df1299ec56ac470b5db6b17
basilwang <ocam@163.com>
2013-05-01 14:49:16 +0800

    mark no use for part of leftSlide code

--- Friday.mvc/apps/malldetail/4.0/other/leftSlide.js
+++ Friday.mvc/apps/malldetail/4.0/other/leftSlide.js
@@ -1,19 +1,19 @@
-﻿KISSY.add("malldetail/other/leftSlide", function (D, E, B, A) {
-    var C = window.g_config;
+﻿KISSY.add("malldetail/other/leftSlide", function (_kissy_D, _dom, _event, _ajax) {
+    var _g_config = window.g_config;
     var G = function (H, I) {
         var J = H.dsrMoreInfo;
-        if (E.get("#J_shopList")) {
-            D.each(D.query("#J_shopList .list"), function (P, L) {
+        if (_dom.get("#J_shopList")) {
+            _kissy_D.each(_kissy_D.query("#J_shopList .list"), function (P, L) {
                 if (J[L]) {
                     var N = (J[L].gradeAvg + "").length > 3 ? (J[L].gradeAvg + "").substr(0, 3) : (J[L].gradeAvg + "");
-                    var M = E.get(".c-value-no", P);
+                    var M = _dom.get(".c-value-no", P);
                     M.className = "c-value-no c-value-" + ((N + "").split(".").join("d"));
                     M.children[0].innerHTML = N;
-                    var O = E.get(".spuDsr-count", P);
-                    O.innerHTML = N + "\u5206(<a href='#J_Reviews' class='J_FocusReviews'>\u7d2f\u8ba1\u8bc4\u4ef7" + J[L].rateTotal + "</a>)";
+                    var O = _dom.get(".spuDsr-count", P);
+                    O.innerHTML = N + "分(<a href='#J_Reviews' class='J_FocusReviews'>累计评价" + J[L].rateTotal + "</a>)";
                     if (I.onReviewClick) {
-                        var K = E.get(".J_FocusReviews", O);
-                        B.on(K, "click", function (Q) {
+                        var K = _dom.get(".J_FocusReviews", O);
+                        _event.on(K, "click", function (Q) {
                             Q.preventDefault();
                             I.onReviewClick()
                         })
@@ -21,55 +21,55 @@
                 }
             })
         } else {
-            D.each(E.query("#J_spuShopList .shop-box"), function (N, K) {
+            _kissy_D.each(_dom.query("#J_spuShopList .shop-box"), function (N, K) {
                 if (J[K]) {
                     var M = (J[K].gradeAvg + "").length > 3 ? (J[K].gradeAvg + "").substr(0, 3) : (J[K].gradeAvg + "");
                     if (K == 0) {
-                        E.get("a", N).innerHTML = '<span class="c-value-no c-value-' + ((M + "").split(".").join("d")) + ' "><em>' + J[K].gradeAvg + "\u5206</em></span>";
-                        E.get(".spuDsr-count", N).innerHTML = M + "\u5206"
+                        _dom.get("a", N).innerHTML = '<span class="c-value-no c-value-' + ((M + "").split(".").join("d")) + ' "><em>' + J[K].gradeAvg + "\u5206</em></span>";
+                        _dom.get(".spuDsr-count", N).innerHTML = M + "\u5206"
                     } else {
-                        var L = E.get(".c-value-no", N);
+                        var L = _dom.get(".c-value-no", N);
                         L.className = "c-value-no c-value-" + ((M + "").split(".").join("d"));
                         L.children[0].innerHTML = M;
-                        E.get(".spuDsr-count", N).innerHTML = M + "\u5206"
+                        _dom.get(".spuDsr-count", N).innerHTML = M + "\u5206"
                     }
                 }
             })
         }
     };
 
-    function F(I) {
-        if (!C.isSpu || E.get("#J_spuShopList") == null) {
+    function _init(_leftSlide_cfg) {
+        if (!_g_config.isSpu || _dom.get("#J_spuShopList") == null) {
             return false
         }
-        var H = E.attr("#J_spuShopList", "dsr-url");
-        A({
+        var H = _dom.attr("#J_spuShopList", "dsr-url");
+        _ajax({
             url: H,
             data: {},
             dataType: "jsonp",
             jsonpCallback: "spuDsrLoad",
             success: function (J) {
-                G(J, I)
+                G(J, _leftSlide_cfg)
             }
         });
-        if (D.cfg("detail").rateEnable) {
-            D.use("malldetail/data/data", function (K, J) {
-                J.onReviewCount(function () {
-                    if (K.cfg("isMeiz")) {
-                        E.show("#J_spuShopList .bts-grade")
+        if (_kissy_D.cfg("detail").rateEnable) {
+            _kissy_D.use("malldetail/data/data", function (_kissy_K, _malldetail_data_data) {
+                _malldetail_data_data.onReviewCount(function () {
+                    if (_kissy_K.cfg("isMeiz")) {
+                        _dom.show("#J_spuShopList .bts-grade")
                     } else {
-                        E.show("#J_shopList .good_com_num")
+                        _dom.show("#J_shopList .good_com_num")
                     }
                 })
             })
         }
     }
     return {
-        init: function (H) {
-            F(H)
+        init: function (_leftSlide_cfg) {
+            _init(_leftSlide_cfg)
         },
         initRelative: function (H) {
-            D.use("malldetail/recommend/a", function (J, I) {
+            _kissy_D.use("malldetail/recommend/a", function (J, I) {
                 I.relativeCommendInit(H.itemDO.categoryId, H.itemDO.brandId, H.loginUserId, H.itemDO.spuId)
             })
         }
