408d4c2348561a00f94a6c06b7e2280786ea3024
basilwang <ocam@163.com>
2013-05-01 18:05:39 +0800

    add breadcrumb controller

---
+++ Friday.mvc/Areas/Merchant/Controllers/ExtensionController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Friday.mvc.Controllers;
+using friday.core.components;
+using Friday.mvc.Areas.Merchant.Models;
+using friday.core.services;
+using friday.core.repositories;
+
+namespace Friday.mvc.Areas.Merchant.Controllers
+{
+    public class ExtensionController : FridayController
+    {
+
+        public ExtensionController(IUserService iUserService, ISystemUserRepository iSystemUserRepository):base(iUserService,iSystemUserRepository)
+        {
+          
+
+        }
+        public ActionResult initExtension()
+        {
+            var breadCrumb=new List<string[]>();
+            breadCrumb.Add(new string[]{"邻居网","http://www.linju.com"});
+            breadCrumb.Add(new string[] { "Nike/&#32784;&#20811;", "http://list.tmall.com/search_product.htm?q=Nike%2F%C4%CD%BF%CB" });
+            var extensionModel = new ExtensionModel()
+            {
+                breadCrumbDO = new BreadCrumbDO()
+                {
+                    breadCrumb = breadCrumb
+                }, 
+                success =true
+                
+            };
+            FormatJsonResult jsonResult = new FormatJsonResult();
+            jsonResult.Data = extensionModel;
+            string json = jsonResult.FormatResult();
+            string script = "jsonpInitExtension(" + json + ")";
+
+            return JavaScript(script);
+        }
+
+    }
+}
---
+++ Friday.mvc/Areas/Merchant/Models/ExtensionModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Friday.mvc.Areas.Merchant.Models
+{
+    public class ExtensionModel
+    {
+        public BreadCrumbDO breadCrumbDO
+        {
+            get;
+            set;
+        }
+        public MealResult mealResult
+        {
+            get;
+            set;
+        }
+        public ShopPromotionDO shopPromotionDO
+        {
+            get;
+            set;
+        }
+        public SpuMaintainerDO spuMaintainerDO
+        {
+            get;
+            set;
+        }
+        public bool success
+        {
+            get;
+            set;
+        }
+    }
+    public class BreadCrumbDO
+    {
+        public List<string[]> breadCrumb
+        {
+            get;
+            set;
+        }
+    }
+    public class MealResult
+    {
+
+    }
+    public class ShopPromotionDO
+    {
+
+    }
+    public class SpuMaintainerDO
+    {
+
+    }
+}
