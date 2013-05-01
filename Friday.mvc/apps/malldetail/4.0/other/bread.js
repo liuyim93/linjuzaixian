ec954aa4ef7b5faca04e44ee344cd1cb0fa4886e
basilwang <ocam@163.com>
2013-05-01 16:46:03 +0800

    rename breadcrumb

--- Friday.mvc/apps/malldetail/4.0/other/bread.js
+++ Friday.mvc/apps/malldetail/4.0/other/bread.js
@@ -1,37 +1,37 @@
-﻿KISSY.add("malldetail/other/bread", function (A) {
-    function B(I, C, G) {
+﻿KISSY.add("malldetail/other/bread", function (_kissy_A) {
+    function B(_breadcrumb_stack, _dom_breadcrumb, _version) {
         var F = [];
-        var H = "";
-        if (C && I.length) {
-            for (var E = 0, D = I.length; E < D; E++) {
-                if (E == D - 1) {
-                    H = ' class="last"'
+        var _last_class_snippet = "";
+        if (_dom_breadcrumb && _breadcrumb_stack.length) {
+            for (var index = 0, _len = _breadcrumb_stack.length; index < _len; index++) {
+                if (index == _len - 1) {
+                    _last_class_snippet = ' class="last"'
                 }
-                if (I[E].length == 2) {
-                    F.push("<li" + H + '><a href="' + I[E][1] + '" target="_blank">' + I[E][0] + "</a></li>")
+                if (_breadcrumb_stack[index].length == 2) {
+                    F.push("<li" + _last_class_snippet + '><a href="' + _breadcrumb_stack[index][1] + '" target="_blank">' + _breadcrumb_stack[index][0] + "</a></li>")
                 } else {
-                    F.push("<li" + H + ">" + I[E][0] + "</li>")
+                    F.push("<li" + _last_class_snippet + ">" + _breadcrumb_stack[index][0] + "</li>")
                 }
             }
             F = F.join("");
-            if (G = "new") {
+            if (_version = "new") {
                 F = '<ul data-spm="1000995">' + F + "</ul>"
             }
-            C.append(F)
+            _dom_breadcrumb.append(F)
         }
     }
     return {
-        init: function (E) {
-            var D = "old";
-            var C = A.one(".crumbs-patch") || A.one(".hdNav");
-            if (!C) {
-                C = A.one("#J_HeaderCrumb");
-                D = "new"
+        init: function (_bread_cfg) {
+            var _version = "old";
+            var _breadcrumb = _kissy_A.one(".crumbs-patch") || _kissy_A.one(".hdNav");
+            if (!_breadcrumb) {
+                _breadcrumb = _kissy_A.one("#J_HeaderCrumb");
+                _version = "new"
             }
-            E = E || {};
-            if (C) {
-                if (E.breadCrumb) {
-                    B(E.breadCrumb, C, D)
+            _bread_cfg = _bread_cfg || {};
+            if (_breadcrumb) {
+                if (_bread_cfg.breadCrumb) {
+                    B(_bread_cfg.breadCrumb, _breadcrumb, _version)
                 }
             }
         }
