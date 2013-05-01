56f8045470425601ab25ff95d6a0413d281ea18d
basilwang <ocam@163.com>
2013-05-01 18:20:52 +0800

    finish merchant extension controller

--- Friday.mvc/apps/malldetail/4.0/other/attributes.js
+++ Friday.mvc/apps/malldetail/4.0/other/attributes.js
@@ -1,8 +1,8 @@
 ﻿KISSY.add("malldetail/other/attributes", function (_kissy_A, _template, _dom) {
     return _kissy_A.mods.attributes = {
-        init: function (F) {
+        init: function (_spuMaintainerDO) {
             var _dom_id_J_AttrList = _kissy_A.get("#J_AttrList");
-            var L = [];
+            var _template_snippet = [];
             var _kv_set = [{
                 k: "providerList",
                 v: '<span data-spm="1000807">产品信息由 {{html}} {{#if len>1}}等{{/if}}发布</span>'
@@ -13,27 +13,27 @@
                 k: "maintainerList",
                 v: '<span data-spm="1000809">感谢由 {{html}} {{#if len>1}}等{{/if}}商家的维护</span>'
             }];
-            if (!_dom_id_J_AttrList || !F) {
+            if (!_dom_id_J_AttrList || !_spuMaintainerDO) {
                 return
             }
             for (var _index = 0; _index < _kv_set.length; _index++) {
                 var _kv_item = _kv_set[_index];
-                if (_kv_item.k in F) {
-                    var H = [];
-                    var K = F[_kv_item.k];
+                if (_kv_item.k in _spuMaintainerDO) {
+                    var _item_snippet = [];
+                    var K = _spuMaintainerDO[_kv_item.k];
                     if (K.length) {
                         for (var _index_j = 0, _len = K.length; _index_j < _len; _index_j++) {
-                            H.push('<a href="' + K[_index_j].shopUrl + '" target="_blank">' + K[_index_j].shopName + "</a>")
+                            _item_snippet.push('<a href="' + K[_index_j].shopUrl + '" target="_blank">' + K[_index_j].shopName + "</a>")
                         }
-                        L.push(_template(_kv_item.v).render({
-                            html: H.join("、"),
+                        _template_snippet.push(_template(_kv_item.v).render({
+                            html: _item_snippet.join("、"),
                             len: _len
                         }))
                     }
                 }
             }
-            if (L.length) {
-                _dom_id_J_AttrList.innerHTML += '<div class="tm-attr-list-ft">' + L.join("，") + "。</div>"
+            if (_template_snippet.length) {
+                _dom_id_J_AttrList.innerHTML += '<div class="tm-attr-list-ft">' + _template_snippet.join("，") + "。</div>"
             }
         }
     }
--- Friday.mvc/apps/malldetail/4.0/other/bread.js
+++ Friday.mvc/apps/malldetail/4.0/other/bread.js
@@ -1,6 +1,6 @@
 ﻿KISSY.add("malldetail/other/bread", function (_kissy_A) {
-    function B(_breadcrumb_stack, _dom_breadcrumb, _version) {
-        var F = [];
+    function _populate_breadcrumb(_breadcrumb_stack, _dom_breadcrumb, _version) {
+        var _breadcrumb_snippet = [];
         var _last_class_snippet = "";
         if (_dom_breadcrumb && _breadcrumb_stack.length) {
             for (var index = 0, _len = _breadcrumb_stack.length; index < _len; index++) {
@@ -8,30 +8,30 @@
                     _last_class_snippet = ' class="last"'
                 }
                 if (_breadcrumb_stack[index].length == 2) {
-                    F.push("<li" + _last_class_snippet + '><a href="' + _breadcrumb_stack[index][1] + '" target="_blank">' + _breadcrumb_stack[index][0] + "</a></li>")
+                    _breadcrumb_snippet.push("<li" + _last_class_snippet + '><a href="' + _breadcrumb_stack[index][1] + '" target="_blank">' + _breadcrumb_stack[index][0] + "</a></li>")
                 } else {
-                    F.push("<li" + _last_class_snippet + ">" + _breadcrumb_stack[index][0] + "</li>")
+                    _breadcrumb_snippet.push("<li" + _last_class_snippet + ">" + _breadcrumb_stack[index][0] + "</li>")
                 }
             }
-            F = F.join("");
+            _breadcrumb_snippet = _breadcrumb_snippet.join("");
             if (_version = "new") {
-                F = '<ul data-spm="1000995">' + F + "</ul>"
+                _breadcrumb_snippet = '<ul data-spm="1000995">' + _breadcrumb_snippet + "</ul>"
             }
-            _dom_breadcrumb.append(F)
+            _dom_breadcrumb.append(_breadcrumb_snippet)
         }
     }
     return {
-        init: function (_bread_cfg) {
+        init: function (_breadcrumbDO) {
             var _version = "old";
             var _breadcrumb = _kissy_A.one(".crumbs-patch") || _kissy_A.one(".hdNav");
             if (!_breadcrumb) {
                 _breadcrumb = _kissy_A.one("#J_HeaderCrumb");
                 _version = "new"
             }
-            _bread_cfg = _bread_cfg || {};
+            _breadcrumbDO = _breadcrumbDO || {};
             if (_breadcrumb) {
-                if (_bread_cfg.breadCrumb) {
-                    B(_bread_cfg.breadCrumb, _breadcrumb, _version)
+                if (_breadcrumbDO.breadCrumb) {
+                    _populate_breadcrumb(_breadcrumbDO.breadCrumb, _breadcrumb, _version)
                 }
             }
         }
