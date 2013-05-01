828a4016f2688d2dca7cfad4d186bc7822bf4b6c
basilwang <ocam@163.com>
2013-05-01 10:09:03 +0800

    adjust thumb viewer

--- Friday.mvc/apps/malldetail/4.0/sku/thumbViewer.js
+++ Friday.mvc/apps/malldetail/4.0/sku/thumbViewer.js
@@ -1,4 +1,4 @@
-﻿KISSY.add("malldetail/sku/thumbViewer", function (_kissy, _dom, _event, _imagezoom, _json, _malldetail_common_util) {
+﻿KISSY.add("malldetail/sku/thumbViewer", function (_kissy, _dom, _event, _imagezoom_t, _json, _malldetail_common_util) {
     var _body = document.body,
         _dom_id_J_ImgBooth,
         _dom_id_J_UlThumb,
@@ -14,7 +14,7 @@
             return
         }
         var _imagezoom_cfg = { imageNode: _dom_id_J_ImgBooth, width: _size_config.bigSize[0], height: _size_config.bigSize[1], bigImageWidth: 800, bigImageHeight: 800, align: { node: _dom.parent(_dom_id_J_ImgBooth), points: ["tr", "tl"], offset: [10, -1] }, hasZoom: !("ontouchstart" in document), preload: false, showIcon: true };
-        var _imagezoom = new _imagezoom(_imagezoom_cfg);
+        var _imagezoom = new _imagezoom_t(_imagezoom_cfg);
         if (_imagezoom.lensIcon) {
             _event.on(_imagezoom.lensIcon, "mouseenter", function () {
                 _dom.hide(this)
@@ -41,7 +41,9 @@
     }
     function _remove_img_size_to_url(_img) {
         var _url = _img.src || _dom.attr(_img, "data-ks-lazyload");
-        return _url.replace(new RegExp("_" + _size_config.smallSize.join("x") + "\\.(jpg|png|gif)$"), "")
+        //return _url.replace(new RegExp("_" + _size_config.smallSize.join("x") + "\\.(jpg|png|gif)$"), "")
+        //2013-05-01 basilwang don't filter _60*60
+        return _url.replace(new RegExp("\\.(jpg|png|gif)$"), "")
     }
     return { init: function () {
         var _thumbviewer = this;
