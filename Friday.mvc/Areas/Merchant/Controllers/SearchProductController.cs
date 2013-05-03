f1b500abe85f1f2c9bc69b27ad3a8bcada8f5653
pangfuxing <pang_fu_xing@126.com>
2013-05-03 16:07:56 +0800

    add  SearchProduct/Index

---
+++ Friday.mvc/Areas/Merchant/Controllers/SearchProductController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using friday.core.services;
+using friday.core.domain;
+using Friday.mvc.Models;
+using friday.core;
+using MvcPaging;
+namespace Friday.mvc.Areas.Merchant.Controllers
+{
+    public class SearchProductController : Controller
+    {
+        //
+        // GET: /Merchant/Search/
+        private IMerchantService iMerchantService;
+        private IMerchantGoodsTypeService iMerchantGoodsTypeService;
+        private IRestaurantService iRestaurantService;
+        private IRentService iRentService;
+        private IShopService iShopService;
+        private IFoodService iFoodService;
+        private IHouseService iHouseService;
+        private ICommodityService iCommodityService;
+
+        public SearchProductController(IMerchantService iMerchantService, IMerchantGoodsTypeService iMerchantGoodsTypeService, IRestaurantService iRestaurantService, IRentService iRentService, IShopService iShopService, ICommodityService iCommodityService, IHouseService iHouseService, IFoodService iFoodService)
+        {
+            this.iMerchantService = iMerchantService;
+            this.iMerchantGoodsTypeService = iMerchantGoodsTypeService;
+            this.iRentService = iRentService;
+            this.iRestaurantService = iRestaurantService;
+            this.iShopService = iShopService;
+            this.iCommodityService = iCommodityService;
+            this.iFoodService = iFoodService;
+            this.iHouseService = iHouseService;
+        }
+        public ActionResult SearchGoods()
+        {
+                   
+
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+         
+
+            return View();
+        }
+
+    }
+}
