b1a1e6af635176f8af10e2a50bdd1eb0ce1dc45f
wanghaichuan <haichuan300@163.com>
2013-05-04 16:36:02 +0800

    add CartPay

---
+++ Friday.mvc/Areas/CartPay/CartPayAreaRegistration.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace Friday.mvc.Areas.CartPay
+{
+    public class CartPayAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "CartPay";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+         name: "mycartpay",
+         url: "cartpay/mycartpay.htm",
+         defaults: new { area = "CartPay", controller = "Home", action = "MyCartPay", id = UrlParameter.Optional }
+        );
+            context.MapRoute(
+                "CartPay_default",
+                "CartPay/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Friday.mvc/Areas/CartPay/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Friday.mvc.Areas.CartPay.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /CartPay/Home/
+
+        public ActionResult MyCartPay()
+        {
+            return View();
+        }
+
+    }
+}
