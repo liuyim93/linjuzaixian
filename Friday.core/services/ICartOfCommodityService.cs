063d7334ec401b70c5313e976e690836c0f7d622
wanghaichuan <haichuan300@163.com>
2013-05-07 10:57:13 +0800

    Add CartOfCommodity repository and services

---
+++ Friday.core/services/ICartOfCommodityService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using friday.core.components;
+using friday.core.domain;
+
+namespace friday.core.services
+{
+    public interface ICartOfCommodityService
+    {
+        CartOfCommodity Load(string id);
+        void Save(CartOfCommodity CartOfCommodity);
+        void Update(CartOfCommodity CartOfCommodity);
+        void Delete(string id);
+        CartOfCommodity getCartOfCommodityByShoppingCart(string ShoppingCartID);
+    }
+}
