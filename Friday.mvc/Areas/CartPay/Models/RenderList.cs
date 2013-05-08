0e4929c34f2c9c4cb66cf1eea8204117384ce3e4
wanghaichuan <haichuan300@163.com>
2013-05-08 11:58:09 +0800

    Add RenderList Model

---
+++ Friday.mvc/Areas/CartPay/Models/RenderList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Friday.mvc.Areas.CartPay.Models
+{
+    public class RenderList
+    {
+        public RenderList()
+        {
+            renderItems = new List<RenderItem>();
+        }
+
+        public List<RenderItem> renderItems
+        {
+            get;
+            set;
+        }
+    }
+    public class RenderItem
+    {
+        public string area_url
+        {
+            get;
+            set;
+        }
+        public string id
+        {
+            get;
+            set;
+        }
+        public string img
+        {
+            get;
+            set;
+        }
+        public string title
+        {
+            get;
+            set;
+        }
+        public double price
+        {
+            get;
+            set;
+        }
+        public string url
+        {
+            get;
+            set;
+        }
+    }
+}
