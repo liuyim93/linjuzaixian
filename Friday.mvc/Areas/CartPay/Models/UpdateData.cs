2d9423d39cb6bc484d761d94944f3685c438d2d3
wanghaichuan <haichuan300@163.com>
2013-05-08 09:23:03 +0800

    Add UpdateData model

---
+++ Friday.mvc/Areas/CartPay/Models/UpdateData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Friday.mvc.Areas.CartPay.Models
+{
+    public class UpdateData
+    {
+        public bool success
+        {
+            get;
+            set;
+        }
+        public globalData globalData
+        {
+            get;
+            set;
+        }
+        public List<UpdateListItem> list
+        {
+            get;
+            set;
+        }
+    }
+
+    public class UpdateOrderItem
+    {
+        public string id
+        {
+            get;
+            set;
+        }
+        public price price
+        {
+            get;
+            set;
+        }
+    }
+
+    public class UpdateListItem
+    {
+        public string id
+        {
+            get;
+            set;
+        }
+        public List<UpdateOrderItem> orders
+        {
+            get;
+            set;
+        }
+    }
+}
