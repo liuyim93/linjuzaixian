230ccfe8a6ff23a709fa13bcc24ac8d029c55431
basilwang <ocam@163.com>
2013-05-20 11:02:17 +0800

    small changes

--- Friday.mvc/apps/tmallsearch/1.0/mods/filter.js
+++ Friday.mvc/apps/tmallsearch/1.0/mods/filter.js
@@ -1,32 +1,32 @@
-﻿KISSY.add(V + "/mods/filter", function (J, Y) {
-    var R = J.DOM,
-		Q = J.Event,
-		i = document,
-		I = window,
-		W = J.UA.ie == 6,
-		d = J.get(".main"),
-		K = J.get("#J_Filter"),
-		G = J.get("#J_FForm"),
-		P = "filter-fix",
-		M = J.get("#J_FPrice"),
-		C = J.query(".j_FPInput"),
-		U = J.get("#J_FPEnter"),
-		F = J.get("#J_FPCancel"),
-		H = "fPrice-hover",
-		Z = J.get("#J_FOriginArea"),
-		O = J.get("#J_FOAInput"),
-		b = J.get("form", Z),
-		a = J.get("button", Z),
-		e = J.get("#J_FDestArea"),
-		B = J.get("#J_DestAreaForm"),
-		g = "fA-text",
-		T = "fA-list",
-		A = "fAll-cities",
-		L = "fAl-cur",
-		N = "fAll-cities-cur",
-		c = new Y(),
-		h = J.get("#J_FMenu"),
-		X = J.get(".j_FMcExpand");
+﻿KISSY.add(V + "/mods/filter", function (_kissy_J, _city_codes) {
+    var _dom = _kissy_J.DOM,
+		_event = _kissy_J.Event,
+		_document = document,
+		_window = window,
+		_is_ie6 = _kissy_J.UA.ie == 6,
+		_dom_class_main = _kissy_J.get(".main"),
+		_dom_id_J_Filter = _kissy_J.get("#J_Filter"),
+		_dom_id_J_FForm = _kissy_J.get("#J_FForm"),
+		_STR_FILTER_FIX = "filter-fix",
+		_dom_id_J_FPrice = _kissy_J.get("#J_FPrice"),
+		_dom_class_j_FPInput = _kissy_J.query(".j_FPInput"),
+		_dom_id_J_FPEnter = _kissy_J.get("#J_FPEnter"),
+		_dom_id_J_FPCancel = _kissy_J.get("#J_FPCancel"),
+		_STR_FPRICE_HOVER = "fPrice-hover",
+		_dom_id_J_FOriginArea = _kissy_J.get("#J_FOriginArea"),
+		_dom_id_J_FOAInput = _kissy_J.get("#J_FOAInput"),
+		_dom_form_in_J_FOriginArea = _kissy_J.get("form", _dom_id_J_FOriginArea),
+		_dom_button_in_J_FOriginArea = _kissy_J.get("button", _dom_id_J_FOriginArea),
+		_dom_id_J_FDestArea = _kissy_J.get("#J_FDestArea"),
+		_dom_id_J_DestAreaForm = _kissy_J.get("#J_DestAreaForm"),
+		_STR_FA_TEXT = "fA-text",
+		_STR_FA_LIST = "fA-list",
+		_STR_FALL_CITIES = "fAll-cities",
+		_STR_FAL_CUR = "fAl-cur",
+		_STR_FALL_CITIES_CUR = "fAll-cities-cur",
+		_city_codes_imp = new _city_codes(),
+		_dom_id_J_FMenu = _kissy_J.get("#J_FMenu"),
+		_dom_class_j_FMcExpand = _kissy_J.get(".j_FMcExpand");
 
     function f() {
         if (!(this instanceof f)) {
@@ -35,9 +35,9 @@
         this.isPriceFocus = false;
         this._init()
     }
-    J.augment(f, J.EventTarget, {
+    _kissy_J.augment(f, _kissy_J.EventTarget, {
         _init: function () {
-            if (!K) {
+            if (!_dom_id_J_Filter) {
                 return
             }
             LIST.msg.fire("ie6Hover", {
@@ -60,118 +60,118 @@
 					S = j.match(new RegExp("\\b" + k + "=(\\d+)\\b"));
                 S && (E.value = S[1])
             };
-            D(C[0], "start_price");
-            D(C[1], "end_price")
+            D(_dom_class_j_FPInput[0], "start_price");
+            D(_dom_class_j_FPInput[1], "end_price")
         },
         _priceEvent: function () {
-            if (!M) {
+            if (!_dom_id_J_FPrice) {
                 return
             }
             var D = this,
 				E = null;
-            Q.on(C, "blur", function () {
+            _event.on(_dom_class_j_FPInput, "blur", function () {
                 E = setTimeout(function () {
                     D.isPriceFocus = false;
-                    R.removeClass(M, H)
+                    _dom.removeClass(_dom_id_J_FPrice, _STR_FPRICE_HOVER)
                 }, 300)
             });
-            Q.on(C, "focus", function () {
+            _event.on(_dom_class_j_FPInput, "focus", function () {
                 clearTimeout(E);
                 D.isPriceFocus = true;
-                R.addClass(M, H)
+                _dom.addClass(_dom_id_J_FPrice, _STR_FPRICE_HOVER)
             });
-            Q.on(C, "keyup", function () {
+            _event.on(_dom_class_j_FPInput, "keyup", function () {
                 var S = this.value;
                 if (!/^\d+\.?\d*$/.test(S)) {
                     S = Math.abs(parseFloat(S));
                     this.value = isNaN(S) ? "" : S
                 }
             });
-            Q.on(U, "click", function (S) {
-                G.submit();
-                J.log(G);
+            _event.on(_dom_id_J_FPEnter, "click", function (S) {
+                _dom_id_J_FForm.submit();
+                _kissy_J.log(_dom_id_J_FForm);
                 S.preventDefault()
             });
-            Q.on(F, "click", function (S) {
-                R.val(C, "");
-                C[0].focus();
+            _event.on(_dom_id_J_FPCancel, "click", function (S) {
+                _dom.val(_dom_class_j_FPInput, "");
+                _dom_class_j_FPInput[0].focus();
                 S.preventDefault()
             });
-            Q.on(K, "keypress", function (S) {
+            _event.on(_dom_id_J_Filter, "keypress", function (S) {
                 if (S.keyCode == 13 && D.isPriceFocus) {
-                    G.submit()
+                    _dom_id_J_FForm.submit()
                 }
             })
         },
         _originAreaEvent: function () {
-            if (!O) {
+            if (!_dom_id_J_FOAInput) {
                 return
             }
-            var D = R.attr(O, "data-val") || "";
-            if (O.value == "") {
-                O.value = D
+            var D = _dom.attr(_dom_id_J_FOAInput, "data-val") || "";
+            if (_dom_id_J_FOAInput.value == "") {
+                _dom_id_J_FOAInput.value = D
             }
-            Q.on(O, "focus", function () {
-                if (O.value == D) {
-                    O.value = ""
+            _event.on(_dom_id_J_FOAInput, "focus", function () {
+                if (_dom_id_J_FOAInput.value == D) {
+                    _dom_id_J_FOAInput.value = ""
                 }
             });
-            Q.on(O, "blur", function () {
-                if (O.value == "") {
-                    O.value = D
+            _event.on(_dom_id_J_FOAInput, "blur", function () {
+                if (_dom_id_J_FOAInput.value == "") {
+                    _dom_id_J_FOAInput.value = D
                 }
             });
-            Q.on(J.query("a", Z), "click", function (E) {
+            _event.on(_kissy_J.query("a", _dom_id_J_FOriginArea), "click", function (E) {
                 E.preventDefault();
-                var S = R.attr(this, "data-val") || R.html(this);
-                O.value = S == "\u6240\u6709\u5730\u533a" ? "" : S;
-                b.submit()
+                var S = _dom.attr(this, "data-val") || _dom.html(this);
+                _dom_id_J_FOAInput.value = S == "\u6240\u6709\u5730\u533a" ? "" : S;
+                _dom_form_in_J_FOriginArea.submit()
             });
-            Q.on(a, "click", function () {
-                if (O.value == D) {
-                    O.value = ""
+            _event.on(_dom_button_in_J_FOriginArea, "click", function () {
+                if (_dom_id_J_FOAInput.value == D) {
+                    _dom_id_J_FOAInput.value = ""
                 }
             })
         },
         _updateDestAreaBoxHtml: function (j, S, E) {
             var l = ['<ul class="fAl-loc">'];
             for (var D in S) {
-                l[l.length] = "<li" + ((E == D) ? ' class="' + L + '"' : "") + '><a href="" code="' + D + '" atpanel="3,' + S[D] + ',,,spu-toloc,20,toloc,">' + S[D] + "</a></li>"
+                l[l.length] = "<li" + ((E == D) ? ' class="' + _STR_FAL_CUR + '"' : "") + '><a href="" code="' + D + '" atpanel="3,' + S[D] + ',,,spu-toloc,20,toloc,">' + S[D] + "</a></li>"
             }
             l[l.length] = "</ul>";
-            R.html(j, l.join(""))
+            _dom.html(j, l.join(""))
         },
         _data: {},
         _destAreaInit: function () {
-            if (!e) {
+            if (!_dom_id_J_FDestArea) {
                 return
             }
-            var D = R.get("." + T, e),
-				m = R.get("." + g, e),
-				S = R.query("a", D),
-				s = R.val(B.area_code),
-				j = c[s];
+            var D = _dom.get("." + _STR_FA_LIST, _dom_id_J_FDestArea),
+				m = _dom.get("." + _STR_FA_TEXT, _dom_id_J_FDestArea),
+				S = _dom.query("a", D),
+				s = _dom.val(_dom_id_J_DestAreaForm.area_code),
+				j = _city_codes_imp[s];
             if (!j) {
                 var n = s.replace(/([\d]{2})\d+/g, "$10000"),
-					E = c[n],
+					E = _city_codes_imp[n],
 					q = E && E.length == 2 ? E[1] : null,
 					l;
                 if (q) {
                     for (var k = 0, o = S.length; k < o; k++) {
                         var p = S[k];
-                        if (R.attr(p, "code") == n) {
+                        if (_dom.attr(p, "code") == n) {
                             var r = p.parentNode;
-                            l = R.next(r, "." + A);
+                            l = _dom.next(r, "." + _STR_FALL_CITIES);
                             this._updateDestAreaBoxHtml(l, q, s);
-                            if (!R.get("i", p)) {
-                                R.append(R.create('<i class="f-ico-triangle-mt" />'), p)
+                            if (!_dom.get("i", p)) {
+                                _dom.append(_dom.create('<i class="f-ico-triangle-mt" />'), p)
                             }
-                            R.addClass(r, L);
-                            R.addClass(l, N);
-                            R.html(m, q[s]);
+                            _dom.addClass(r, _STR_FAL_CUR);
+                            _dom.addClass(l, _STR_FALL_CITIES_CUR);
+                            _dom.html(m, q[s]);
                             this._data.lastBox = l;
                             this._data.lastLi = r;
-                            this._data.lastChildLi = R.get("." + L, l);
+                            this._data.lastChildLi = _dom.get("." + _STR_FAL_CUR, l);
                             break
                         }
                     }
@@ -180,8 +180,8 @@
                 for (var k = 0, o = S.length; k < o; k++) {
                     var p = S[k],
 						r = p.parentNode;
-                    if (R.attr(p, "code") == s) {
-                        R.addClass(r, L);
+                    if (_dom.attr(p, "code") == s) {
+                        _dom.addClass(r, _STR_FAL_CUR);
                         this._data.lastLi = r;
                         break
                     }
@@ -189,72 +189,72 @@
             }
         },
         _destAreaEvent: function () {
-            if (!e) {
+            if (!_dom_id_J_FDestArea) {
                 return
             }
             var D = this,
-				j = R.get("." + T, e),
+				j = _dom.get("." + _STR_FA_LIST, _dom_id_J_FDestArea),
 				S = D._data.lastChildLi,
 				k = D._data.lastLi,
 				E = D._data.lastBox;
-            Q.delegate(j, "click", "a", function (q) {
+            _event.delegate(j, "click", "a", function (q) {
                 var m = q.currentTarget,
 					l = m.parentNode,
-					n = R.attr(m, "code"),
+					n = _dom.attr(m, "code"),
 					p = [],
 					o = null;
-                if (R.hasClass(l, A)) {
+                if (_dom.hasClass(l, _STR_FALL_CITIES)) {
                     return
                 }
-                R.addClass(l, L);
-                n && (p = c[n] || []);
+                _dom.addClass(l, _STR_FAL_CUR);
+                n && (p = _city_codes_imp[n] || []);
                 l.grade = p.length !== 0 ? 1 : 2;
-                E && l.grade == 1 && R.removeClass(E, N);
+                E && l.grade == 1 && _dom.removeClass(E, _STR_FALL_CITIES_CUR);
                 p.length == 2 && (function () {
-                    if (o = R.next(l, "." + A)) {
+                    if (o = _dom.next(l, "." + _STR_FALL_CITIES)) {
                         D._updateDestAreaBoxHtml(o, p[1]);
-                        if (!R.get("i", m)) {
-                            R.append(R.create('<i class="f-ico-triangle-mt" />'), m)
+                        if (!_dom.get("i", m)) {
+                            _dom.append(_dom.create('<i class="f-ico-triangle-mt" />'), m)
                         }
-                        R.addClass(o, N);
+                        _dom.addClass(o, _STR_FALL_CITIES_CUR);
                         E = o
                     }
                 })();
                 switch (l.grade) {
                     case 1:
                         if (k != l) {
-                            R.removeClass(k, L);
+                            _dom.removeClass(k, _STR_FAL_CUR);
                             k = l
                         }
                         break;
                     case 2:
                         if (S != l) {
-                            R.removeClass(S, L);
+                            _dom.removeClass(S, _STR_FAL_CUR);
                             S = l
                         }
                         break
                 }
                 q.preventDefault();
                 if (p.length !== 2) {
-                    R.val(B.area_code, n);
-                    B.submit()
+                    _dom.val(_dom_id_J_DestAreaForm.area_code, n);
+                    _dom_id_J_DestAreaForm.submit()
                 }
             })
         },
         _menuEvent: function () {
-            if (!h) {
+            if (!_dom_id_J_FMenu) {
                 return
             }
-            Q.on(J.query("input", h), "click", function () {
-                G.submit()
+            _event.on(_kissy_J.query("input", _dom_id_J_FMenu), "click", function () {
+                _dom_id_J_FForm.submit()
             })
         },
         _menuInit: function () {
-            if (!h) {
+            if (!_dom_id_J_FMenu) {
                 return
             }
             var S = "drop",
-				E = R.children(h, function (k) {
+				E = _dom.children(_dom_id_J_FMenu, function (k) {
 				    return k.tagName == "INPUT" && k.type == "checkbox"
 				}),
 				D = E.length;
@@ -265,52 +265,52 @@
                 }
             }
             LIST.msg.fire("expand", {
-                el: X,
+                el: _dom_class_j_FMcExpand,
                 classname: "fMenu",
                 status: S
             })
         },
         _fix: function () {
-            var S = R.create('<div id="J_FilterPlaceholder" />', {
+            var S = _dom.create('<div id="J_FilterPlaceholder" />', {
                 css: {
-                    height: R.outerHeight(K, true),
+                    height: _dom.outerHeight(_dom_id_J_Filter, true),
                     display: "none"
                 }
             });
-            R.insertAfter(S, K);
+            _dom.insertAfter(S, _dom_id_J_Filter);
             this.needFixTop = true;
-            var E = Math.max(R.offset(K).top, 130),
+            var E = Math.max(_dom.offset(_dom_id_J_Filter).top, 130),
 				D = function () {
-				    var j = R.scrollTop(I);
+				    var j = _dom.scrollTop(_window);
 				    if (j > E) {
-				        if (!R.hasClass(K, P)) {
-				            R.addClass(K, P);
-				            R.show(S)
+				        if (!_dom.hasClass(_dom_id_J_Filter, _STR_FILTER_FIX)) {
+				            _dom.addClass(_dom_id_J_Filter, _STR_FILTER_FIX);
+				            _dom.show(S)
 				        }
-				        if (W) {
-				            R.css(K, "top", j - 10 - R.offset(d).top)
+				        if (_is_ie6) {
+				            _dom.css(_dom_id_J_Filter, "top", j - 10 - _dom.offset(_dom_class_main).top)
 				        }
 				    } else {
-				        R.removeClass(K, P);
-				        R.hide(S);
-				        E = Math.max(R.offset(K).top, 130)
+				        _dom.removeClass(_dom_id_J_Filter, _STR_FILTER_FIX);
+				        _dom.hide(S);
+				        E = Math.max(_dom.offset(_dom_id_J_Filter).top, 130)
 				    }
 				};
             D();
-            Q.on(I, "scroll", D)
+            _event.on(_window, "scroll", D)
         },
         _tipCtrl: function (S) {
-            if (S || !h) {
+            if (S || !_dom_id_J_FMenu) {
                 return
             }
-            var j = R.query("label", h);
+            var j = _dom.query("label", _dom_id_J_FMenu);
             if (!j.length) {
                 return
             }
             var E = [1, -1, 0, 0],
 				D = E.length;
-            J.query(".j_FTip").each(function (o) {
-                var l = R.attr(o, "data-cfg"),
+            _kissy_J.query(".j_FTip").each(function (o) {
+                var l = _dom.attr(o, "data-cfg"),
 					k = E;
                 if (l) {
                     k = l.split(",");
@@ -320,36 +320,36 @@
                 }
                 var m = j[k[0] - 1];
                 if (m) {
-                    var p = m.offsetLeft + R.outerWidth(m) / 2 - R.outerWidth(o) / 2;
-                    R.css(o, "left", p + "px")
+                    var p = m.offsetLeft + _dom.outerWidth(m) / 2 - _dom.outerWidth(o) / 2;
+                    _dom.css(o, "left", p + "px")
                 }
                 k[1] !== -1 && setTimeout(function () {
-                    R.hide(".j_FTip")
+                    _dom.hide(".j_FTip")
                 }, k[1] < 0 ? 0 : k[1]);
-                k[2] && R.show(R.get(".j_FTCat", o));
-                k[3] && R.show(R.get(".j_FTX", o));
-                R.show(o)
+                k[2] && _dom.show(_dom.get(".j_FTCat", o));
+                k[3] && _dom.show(_dom.get(".j_FTX", o));
+                _dom.show(o)
             })
         },
         _disctOpts: function () {
             var D = "#J_FDisctOpts";
-            if (!R.get(D)) {
+            if (!_dom.get(D)) {
                 return
             }
-            J.UA.ie == 7 && R.query(D + " a").each(function (E) {
+            _kissy_J.UA.ie == 7 && _dom.query(D + " a").each(function (E) {
                 E.setAttribute("hideFocus", "true")
             });
-            Q.delegate(D, "click", "a", function (j) {
+            _event.delegate(D, "click", "a", function (j) {
                 var S = j.target,
 					E = "fDO-cur",
 					k = "fDO-disable";
-                if (R.hasClass(S, k)) {
+                if (_dom.hasClass(S, k)) {
                     return
                 }
-                R.query(D + " a").each(function (l) {
-                    R.removeClass(l, E)
+                _dom.query(D + " a").each(function (l) {
+                    _dom.removeClass(l, E)
                 });
-                R.addClass(S, E)
+                _dom.addClass(S, E)
             })
         }
     });
