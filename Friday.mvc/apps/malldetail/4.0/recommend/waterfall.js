52dc33aa2de7411177dafb4d4c7704a747d29e88
basilwang <ocam@163.com>
2013-05-29 11:00:03 +0800

    重构waterfall脚本（部分)

--- Friday.mvc/apps/malldetail/4.0/recommend/waterfall.js
+++ Friday.mvc/apps/malldetail/4.0/recommend/waterfall.js
@@ -1,7 +1,7 @@
 ﻿/*pub-1|2013-01-25 14:16:03*/
-KISSY.add("malldetail/recommend/waterfall", function (G, O, H, R, I) {
-    var T = document;
-    var L = {};
+KISSY.add("malldetail/recommend/waterfall", function (_kissy_G, _dom, _waterfall, _template, _malldetail_tabbar_tabbar) {
+    var _document = document;
+    var Waterfall = {};
     var E;
     var P;
     var K = 1;
@@ -13,18 +13,18 @@ KISSY.add("malldetail/recommend/waterfall", function (G, O, H, R, I) {
     var U = '<li class="ald-wf-li"><a href="{{url}}" class="ald-wf-pic" target="_blank"><img height="220" src="{{img}}_250x250.jpg"><em class="ald-wf-em"></em><span class="ui-price"><span class="ui-price-icon">&yen;</span>{{price}}</span>{{#if macketPrice}}<span class="ui-price-original"><span class="ui-price-icon">&yen;</span><del>{{macketPrice}}</del></span>{{/if}}</a>{{#each comments as comment}}{{#if comment.content.length<60}}<p><b>{{comment.nick}}</b>{{comment.content}}</p>{{/if}}{{/each}}</li>';
 
     function C(Y, S) {
-        var W = G.mods.tabbarAttr.curTag();
+        var W = _kissy_G.mods.tabbarAttr.curTag();
         var V = W.id ? true : false;
         var Z = setTimeout(function () {
-            O.hide(P)
+            _dom.hide(P)
         }, 2000);
-        O.show(P);
+        _dom.show(P);
         if (D.length) {
             Y(D);
             D = []
         }
-        var X = G.cfg("isDaily") ? "13975762416" : window.g_config.itemId;
-        G.Asyn.add("AldTabWaterfall", {
+        var X = _kissy_G.cfg("isDaily") ? "13975762416" : window.g_config.itemId;
+        _kissy_G.Asyn.add("AldTabWaterfall", {
             //url: "http://ald.taobao.com/recommend.htm",
             url: "http://localhost:7525/Merchant/Detail/Tab_Recommend",
             data: {
@@ -32,7 +32,7 @@ KISSY.add("malldetail/recommend/waterfall", function (G, O, H, R, I) {
                 itemId: X,
                 vid: W.id,
                 curPage: K,
-                refer: T.referrer,
+                refer: _document.referrer,
                 step: 6
             },
             jsonpCallback: "jsonpAldTabWaterfall",
@@ -42,27 +42,27 @@ KISSY.add("malldetail/recommend/waterfall", function (G, O, H, R, I) {
                 if (Z) {
                     clearTimeout(Z)
                 }
-                O.hide(P);
+                _dom.hide(P);
                 if (!b.list || b.list.length == 0) {
                     S();
                     return
                 }
                 K++;
                 var a = [];
-                G.each(b.list, function (d) {
+                _kissy_G.each(b.list, function (d) {
                     d.macketPrice = d.macketPrice || "";
                     d.comments = d.comments || [];
                     if (V && !Q && W.id != d.vid) {
-                        D.push(R(U).render(d))
+                        D.push(_template(U).render(d))
                     } else {
-                        a.push(R(U).render(d))
+                        a.push(_template(U).render(d))
                     }
                 });
                 Y(a);
                 if (D.length) {
                     Q = true;
-                    var c = O.create('<div class="ald-wf-line">以上是' + W.name + '的全部结果，我再看看其他类型的吧！</div><ul id="J_TjWaterfallOther" class="clearfix ald-03040"></ul>');
-                    O.append(c, E);
+                    var c = _dom.create('<div class="ald-wf-line">以上是' + W.name + '的全部结果，我再看看其他类型的吧！</div><ul id="J_TjWaterfallOther" class="clearfix ald-03040"></ul>');
+                    _dom.append(c, E);
                     F.destroy();
                     F = N("#J_TjWaterfallOther")
                 }
@@ -70,7 +70,7 @@ KISSY.add("malldetail/recommend/waterfall", function (G, O, H, R, I) {
                     S();
                     return
                 }
-                if (I.curIndex() == "description") {
+                if (_malldetail_tabbar_tabbar.curIndex() == "description") {
                     F.pause();
                     B = true;
                     return
@@ -80,12 +80,12 @@ KISSY.add("malldetail/recommend/waterfall", function (G, O, H, R, I) {
     }
     function M(S) {
         if (!J) {
-            G.sendImg(S);
+            _kissy_G.sendImg(S);
             J = true
         }
     }
     function N(S) {
-        return new H.Loader({
+        return new Waterfall.Loader({
             container: S,
             load: function (W, V) {
                 return C(W, V)
@@ -96,29 +96,29 @@ KISSY.add("malldetail/recommend/waterfall", function (G, O, H, R, I) {
         })
     }
     function A() {
-        var S = O.create('<ul id="J_TjWaterfall" class="clearfix ald-03040"></ul>');
+        var S = _dom.create('<ul id="J_TjWaterfall" class="clearfix ald-03040"></ul>');
         if (!E) {
-            E = O.create('<div id="J_Tjwrap"></div>')
+            E = _dom.create('<div id="J_Tjwrap"></div>')
         } else {
-            O.remove(O.children(E))
+            _dom.remove(_dom.children(E))
         }
-        O.append(S, E)
+        _dom.append(S, E)
     }
-    G.mix(L, {
+    _kissy_G.mix(Waterfall, {
         init: function () {
             var S = this;
-            I.onSwitch(function (V) {
+            _malldetail_tabbar_tabbar.onSwitch(function (V) {
                 if (V == "J_TabRecommends" && B) {
                     F.resume();
                     B = false
                 }
             });
             A();
-            P = O.create('<div style="height:200px;background-color:#fff" class="ui-loading"></div>');
-            O.append(E, "#J_TabRecommends");
-            O.append(P, "#J_TabRecommends");
+            P = _dom.create('<div style="height:200px;background-color:#fff" class="ui-loading"></div>');
+            _dom.append(E, "#J_TabRecommends");
+            _dom.append(P, "#J_TabRecommends");
             F = N("#J_TjWaterfall");
-            G.on("AttrTagChange", function () {
+            _kissy_G.on("AttrTagChange", function () {
                 S.reload()
             })
         },
@@ -133,7 +133,7 @@ KISSY.add("malldetail/recommend/waterfall", function (G, O, H, R, I) {
             F = N("#J_TjWaterfall")
         }
     });
-    return L
+    return Waterfall
 }, {
     requires: ["dom", "waterfall", "template", "malldetail/tabbar/tabbar", "malldetail/recommend/waterfall.css"]
 });
