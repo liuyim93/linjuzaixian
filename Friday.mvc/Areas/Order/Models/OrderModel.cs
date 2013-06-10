d793ae748950d9ff10422fd0cd16f6fdf020e1f5
wanghaichuan <haichuan300@163.com>
2013-06-10 16:49:48 +0800

    Add model

---
+++ Friday.mvc/Areas/Order/Models/OrderModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using friday.core;
+using friday.core.domain;
+
+namespace Friday.mvc.Models
+{
+    public class OrderModel
+    {
+        public OrderModel()
+        {
+
+        }
+
+        public string orderDataScript { get; set; }
+
+    }
+}
