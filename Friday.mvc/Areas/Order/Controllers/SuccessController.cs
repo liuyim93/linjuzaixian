a0e9554af121ddf483c10cdea87d639f33743129
wanghaichuan <haichuan300@163.com>
2013-06-11 16:11:23 +0800

    add Page success

---
+++ Friday.mvc/Areas/Order/Controllers/SuccessController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Friday.mvc.Areas.Order.Controllers
+{
+    public class SuccessController : Controller
+    {
+        //
+        // GET: /Order/Success/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
