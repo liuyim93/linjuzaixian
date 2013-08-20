e67fa2042edcad7dc1fa4af836b9bfc1d08e9fda
basilwang <ocam@163.com>
2013-08-20 22:11:33 +0800

    重构了slide2 和backtop

--- Friday.mvc/apps/tmall/fp/2012/mods/slide2.js
+++ Friday.mvc/apps/tmall/fp/2012/mods/slide2.js
@@ -1,6 +1,7 @@
 // JScript source code
 KISSY.add("2012/mods/slide2", function (_kissy, _switchable) {
-    var _dom = _kissy.DOM, _event = _kissy.Event; var _window = window; var _is_ie6 = _kissy.UA.ie == 6;
+    var _dom = _kissy.DOM, _event = _kissy.Event; var _window = window;
+    var _is_ie6 = _kissy.UA.ie == 6;
     var _is_under_ie9 = _kissy.UA.ie && _kissy.UA.ie < 9;
     var _closeSlideAutoPlay = !_window.g_config.closeSlideAutoPlay;
     var _closeTriggerAnim = _window.g_config.closeTriggerAnim;
@@ -15,9 +16,9 @@ KISSY.add("2012/mods/slide2", function (_kissy, _switchable) {
     var K = ["mm_12852562_1778064_10953677", "mm_12852562_1778064_10953701"];
     var _str_data_text_src = "data-text-src";
     var _str_data_image = "data-image";
-    var V = /^https?:\/\/\S+$/i;
-    var H = /^https?:\/\/\S+(png|jpg|gif)$/i;
-    var R = /^#[0-9a-fA-F]{6}$/;
+    var _regex_for_url = /^https?:\/\/\S+$/i;
+    var _regex_for_image_url = /^https?:\/\/\S+(png|jpg|gif)$/i;
+    var _regex_for_6characters = /^#[0-9a-fA-F]{6}$/;
     var _config = { contentCls: "mfpSlide-con", navCls: "mfpSlide-nav", activeTriggerCls: "selected", effect: "fade", easing: "easeOut", lazyDataType: "img-src", duration: 0.5, triggerType: "mouse", autoplay: _closeSlideAutoPlay, pauseOnScroll: true, delay: 0.2 };
     function Slide2() {
         var _slide2 = this;
@@ -28,12 +29,13 @@ KISSY.add("2012/mods/slide2", function (_kissy, _switchable) {
     }
     _kissy.augment(Slide2, _kissy.EventTarget,
             { _init: function () {
-                var _slide2 = this; if (!_kissy.get("#" + _str_J_MfpSlide))
+                var _slide2 = this;
+                if (!_kissy.get("#" + _str_J_MfpSlide))
                 { return }
                 var _slide = _slide2.slide = new _switchable.Slide("#" + _str_J_MfpSlide, _config);
                 _window.g_config.Slide = _slide;
                 _is_ie6 && _kissy.ready(function () { _slide2._fixPng24() });
-                _slide.on("beforeSwitch", function(_op){
+                _slide.on("beforeSwitch", function (_op) {
                     _slide2._textAnim(_op);
                     _slide2._lazyLoad(_op)
                 });
@@ -72,59 +74,71 @@ KISSY.add("2012/mods/slide2", function (_kissy, _switchable) {
                     var _slide2 = this;
                     var _slide = _slide2.slide;
                     _kissy.later(function () {
-                        _kissy.each(_kissy.query("." + _str_J_DirectPromo, _slide.content),
+                        _kissy.each(_kissy.query("." + _str_J_DirectPromo, _slide.content /* the parent node of the panels*/),
                                 function (_direct_promo) {
                                     var _a_on_direct_promo = _kissy.get("a", _direct_promo);
                                     !_kissy.get("s", _a_on_direct_promo) && _dom.append(_dom.create("<s>"), _a_on_direct_promo)
                                 })
                     },
                         10);
+                    /*
+                    2013-08-20 basilwang
+                    direct img return format
+                    _content_result
+                    id
+                    link   -> link url
+                    content
+                    0-> img url
+                    1-> img url
+                    2-> 6characters maybe color
+                    */
                     _kissy.each(_content_result_array,
                         function (_content_result) {
                             if (_kissy.inArray(_content_result.id, _origin_content_result)) {
-                                var f = _content_result.content.split(";;");
-                                var d = _content_result.link.split(";;");
-                                var b = _kissy.get("." + _str_j_MSDirect_ + _content_result.id);
-                                if (!(b && V.test(d[0]) && H.test(f[0]) && H.test(f[1]) && R.test(f[3])))
+                                var _content = _content_result.content.split(";;");
+                                var _link = _content_result.link.split(";;");
+                                var _dom_direct = _kissy.get("." + _str_j_MSDirect_ + _content_result.id);
+                                if (!(_dom_direct && _regex_for_url.test(_link[0]) && _regex_for_image_url.test(_content[0]) && _regex_for_image_url.test(_content[1]) && _regex_for_6characters.test(_content[3])))
                                 { return }
-                                var e = _kissy.get("a", b);
-                                var c = _kissy.get("img", _kissy.get("b", b));
-                                e.href = d[0];
-                                if (_dom.hasAttr(b, _str_data_image)) {
-                                    _dom.css(b, "background-color", f[3]);
-                                    _dom.attr(b, _str_data_image, _slide2._optimizeImg(f[0]));
-                                    _is_ie6 ? _slide2._fixPng24(c, f[1]) : _dom.attr(c, _str_data_text_src, f[1])
+                                var _dom_a = _kissy.get("a", _dom_direct);
+                                var _dom_img = _kissy.get("img", _kissy.get("b", _dom_direct));
+                                _dom_a.href = _link[0];
+                                if (_dom.hasAttr(_dom_direct, _str_data_image)) {
+                                    _dom.css(_dom_direct, "background-color", _content[3]);
+                                    _dom.attr(_dom_direct, _str_data_image, _slide2._optimizeImg(_content[0]));
+                                    _is_ie6 ? _slide2._fixPng24(_dom_img, _content[1]) : _dom.attr(_dom_img, _str_data_text_src, _content[1])
                                 }
                                 else {
-                                    _dom.css(b, { "background-color": f[3], "background-image": "url(" + _slide2._optimizeImg(f[0]) + ")" });
-                                    _is_ie6 ? _slide2._fixPng24(c, f[1]) : c.src = f[1]
+                                    _dom.css(_dom_direct, { "background-color": _content[3], "background-image": "url(" + _slide2._optimizeImg(_content[0]) + ")" });
+                                    _is_ie6 ? _slide2._fixPng24(_dom_img, _content[1]) : _dom_img.src = _content[1]
                                 }
                             }
                         })
                 },
-                //2013-02-17 basilwang we don't need _tanxAdd
-                _tanxAd: function () {
-                    _kissy.each(K, function (S) {
-                        _kissy.getScript("http://p.tanx.com/ex?i=" + S,
-                            function () {
-                                var b = _kissy.get("#tanx-a-" + S),
-                                    Z, c, d = 0;
-                                if (!b) { return }
-                                Z = _dom.parent(b, "li");
-                                _kissy.later(function () {
-                                    c = _dom.prev(b, "a");
-                                    if (!c && d < 30) { setTimeout(arguments.callee, 100); d++ }
-                                    else {
-                                        var a = _kissy.get("img", c);
-                                        var e = _kissy.get("a", Z);
-                                        _dom.css(Z, "background-image", "url(" + a.src + ")");
-                                        e.href = c.href; _dom.hide("." + _str_j_SlideTanxAd)
-                                    }
-                                }, 20)
-                            })
-                    })
-                }
-                , _hoverMask: function () {
+                //                //2013-02-17 basilwang we don't need _tanxAdd
+                //                _tanxAd: function () {
+                //                    _kissy.each(K, function (S) {
+                //                        _kissy.getScript("http://p.tanx.com/ex?i=" + S,
+                //                            function () {
+                //                                var b = _kissy.get("#tanx-a-" + S),
+                //                                    Z, c, d = 0;
+                //                                if (!b) { return }
+                //                                Z = _dom.parent(b, "li");
+                //                                _kissy.later(function () {
+                //                                    c = _dom.prev(b, "a");
+                //                                    if (!c && d < 30) { setTimeout(arguments.callee, 100); d++ }
+                //                                    else {
+                //                                        var a = _kissy.get("img", c);
+                //                                        var e = _kissy.get("a", Z);
+                //                                        _dom.css(Z, "background-image", "url(" + a.src + ")");
+                //                                        e.href = c.href; _dom.hide("." + _str_j_SlideTanxAd)
+                //                                    }
+                //                                }, 20)
+                //                            })
+                //                    })
+                //                }
+                //                ,
+                _hoverMask: function () {
                     var _slide2 = this;
                     var _node_ul_class_j_MaskBanner = _kissy.all("." + _str_j_MaskBanner, _slide2.slide.container);
                     _kissy.each(_node_ul_class_j_MaskBanner, function (_node) {
@@ -149,25 +163,33 @@ KISSY.add("2012/mods/slide2", function (_kissy, _switchable) {
                         })
                     })
                 },
-                _fixPng24: function (Z, a) {
+                _fixPng24: function (_dom_img, _img_url) {
                     var _slide = this.slide;
-                    var b = function (c) {
-                        _dom.css(c, { filter: 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' + (a || _dom.attr(c, _str_data_text_src) || c.src) + '",enabled="true", sizinMethod="scale");' });
-                        c.src = "http://www.linjuzaixian.com/Images/spaceball.gif";
-                        _dom.show(c)
+                    var _set_filter_and_spaceball = function (_dom_img_t) {
+                        _dom.css(_dom_img_t, { filter: 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' + (_img_url || _dom.attr(_dom_img_t, _str_data_text_src) || _dom_img_t.src) + '",enabled="true", sizinMethod="scale");' });
+                        _dom_img_t.src = "http://www.linjuzaixian.com/Images/spaceball.gif";
+                        _dom.show(_dom_img_t)
                     };
-                    Z ? b(Z) : _kissy.each(_slide.panels, function (d) { var c = _kissy.get("b", d), e; if (c) { if (e = _kissy.get("img", c)) { b(e) } _dom.show(c) } })
+                    _dom_img ? _set_filter_and_spaceball(_dom_img) : _kissy.each(_slide.panels, function (_panel) {
+                        var _dom_b = _kissy.get("b", _panel), _dom_img;
+                        if (_dom_b) {
+                            if (_dom_img = _kissy.get("img", _dom_b)) {
+                                _set_filter_and_spaceball(_dom_img)
+                            }
+                            _dom.show(_dom_b)
+                        }
+                    })
                 },
-                _optimizeImg: function (S) {
-                    var Z = "_q90.jpg";
-                    if (!g_config.closeOptSlideImg) { S += Z } return S
+                _optimizeImg: function (_img_url) {
+                    var _postfix = "_q90.jpg";
+                    if (!g_config.closeOptSlideImg) { _img_url += _postfix } return _img_url
                 },
-                 _triggerAnim: function () {
+                _triggerAnim: function () {
                     var h = this.slide; var Z; var k; var a; var m; var d, j; var e = 0; var g;
                     function S(o) {
                         var n = "rotate(" + o + "deg)";
                         a.css({ "-webkit-transform": n, "-moz-transform": n, "-o-transform": n })
-                     }
+                    }
                     function i(o) {
                         if (g) { return } var n = o || (5000 / 180); clearInterval(d); d = setInterval(function (p) {
                             if (e < 360) { e += 2; S(e) } if (e > 180)
@@ -176,7 +198,7 @@ KISSY.add("2012/mods/slide2", function (_kissy, _switchable) {
                     }
                     function c() {
                         clearInterval(d)
-                     }
+                    }
                     function f() {
                         k.removeClass("move");
                         a.removeClass("move");
--- Friday.mvc/apps/tmall/mui/backtop/js/backtop.js
+++ Friday.mvc/apps/tmall/mui/backtop/js/backtop.js
@@ -1,27 +1,27 @@
 ﻿/*pub-1|2013-01-17 17:47:50*/
-(function (K, J, N) {
-    var Q = J.Event,
-		B = J.DOM,
-		C = J.Anim,
-		L = J.UA.ie && J.UA.ie < 9,
-		I = L && J.UA.ie == 6,
-		P = "J_ScrollTopBtn",
-		F = "ui-scrolltop-hidden";
+(function (_window, _kissy_J, _document) {
+    var _event = _kissy_J.Event,
+		_dom = _kissy_J.DOM,
+		_anim = _kissy_J.Anim,
+		_is_ie = _kissy_J.UA.ie && _kissy_J.UA.ie < 9,
+		_is_ie6 = _is_ie && _kissy_J.UA.ie == 6,
+		_str_J_ScrollTopBtn = "J_ScrollTopBtn",
+		_str_ui_scrolltop_hidden = "ui-scrolltop-hidden";
 
-    function O(D) {
-        var A = N.getElementsByTagName("script"),
-			S = A[A.length - 1].src || "",
-			E = new RegExp("\\?(?:.+&)?" + D + "=([^&]*)(&|$)", "i"),
-			R = S.match(E);
-        return (R != null) ? unescape(R[1]) : null
+    function _get_value_from_url(_key) {
+        var _script_dom = _document.getElementsByTagName("script"),
+			_url = _script_dom[_script_dom.length - 1].src || "",
+			_regex = new RegExp("\\?(?:.+&)?" + _key + "=([^&]*)(&|$)", "i"),
+			_matched_set = _url.match(_regex);
+        return (_matched_set != null) ? unescape(_matched_set[1]) : null
     }
-    function M(R) {
-        var A = N.getElementsByTagName("script"),
-			D = A[A.length - 1],
-			E = D.getAttribute("data-st-" + R);
-        return E ? E : O(R)
+    function _get_value_from_attribute(_key) {
+        var _script_dom = _document.getElementsByTagName("script"),
+			_url = _script_dom[_script_dom.length - 1],
+			_attribute = _url.getAttribute("data-st-" + _key);
+        return _attribute ? _attribute : _get_value_from_url(_key)
     }
-    var H = {
+    var _options = {
         bottom: 200,
         right: 10,
         height: "60px",
@@ -29,120 +29,120 @@
         baseLine: 400,
         isAnim: false,
         type: 1
-    }, G = {
+    }, _BackTop = {
         scrollEl: null,
         getValue: function () {
-            return K.pageYOffset || N.body.scrollTop || N.documentElement.scrollTop
+            return _window.pageYOffset || _document.body.scrollTop || _document.documentElement.scrollTop
         },
-        updateValue: function (D, A, R, E) {
-            K.scrollTo(0, -R * (D /= E) * (D - 2) + A)
+        updateValue: function (_step_index, _scrollTop, _offset, _step) {
+            _window.scrollTo(0, -_offset * (_step_index /= _step) * (_step_index - 2) + _scrollTop)
         },
-        scrollTop: function (V) {
-            var A = this,
-				S = A.getValue(),
-				U = null,
-				D = 5,
-				T = 0,
-				E = 0,
-				R = parseInt(S / 150);
-            if (V !== S) {
-                E = V - S;
-                U = setInterval(function () {
-                    if (T <= R) {
-                        A.updateValue(T, S, E, R);
-                        T++
+        scrollTop: function (_value) {
+            var _backtop = this,
+				_scrollTop = _backtop.getValue(),
+				_fn = null,
+				_interval_time = 5,
+				_step_index = 0,
+				_offset = 0,
+				_step = parseInt(_scrollTop / 150);
+            if (_value !== _scrollTop) {
+                _offset = _value - _scrollTop;
+                _fn = setInterval(function () {
+                    if (_step_index <= _step) {
+                        _backtop.updateValue(_step_index, _scrollTop, _offset, _step);
+                        _step_index++
                     } else {
-                        clearInterval(U)
+                        clearInterval(_fn)
                     }
-                }, D)
+                }, _interval_time)
             }
         },
         addCss: function () {
-            var A = ".ui-scrolltop{ display:block; position:fixed; _position:absolute; right:10px; bottom:200px; z-index: 9999; height:60px; width:60px; text-indent:-999999px; overflow: hidden; background:#000 url(http://www.linjuzaixian.com/Images/up.png) 18px 21px no-repeat; *background-image:url(http://www.linjuzaixian.com/Images/backtop.png);opacity:0.5; filter:alpha(opacity=50); cursor: pointer; -moz-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  -webkit-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  -o-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  transition: opacity 0.3s ease-in,opacity 0.3s ease-out; } .ui-scrolltop:hover{ opacity:1; filter:alpha(opacity=100); -moz-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  -webkit-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  -o-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  transition: opacity 0.3s ease-in,opacity 0.3s ease-out; } .ui-scrolltop-hidden{ visibility: hidden; _visibility:visible;_display:none;opacity:0; filter:alpha(opacity=0); -moz-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  -webkit-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  -o-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  transition: opacity 0.3s ease-in,opacity 0.3s ease-out; }.ui-scrolltop-wb{ background-position:-25px 21px; background-color:#fff; }";
-            return A
+            var _str_class = ".ui-scrolltop{ display:block; position:fixed; _position:absolute; right:10px; bottom:200px; z-index: 9999; height:60px; width:60px; text-indent:-999999px; overflow: hidden; background:#000 url(http://www.linjuzaixian.com/Images/up.png) 18px 21px no-repeat; *background-image:url(http://www.linjuzaixian.com/Images/backtop.png);opacity:0.5; filter:alpha(opacity=50); cursor: pointer; -moz-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  -webkit-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  -o-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  transition: opacity 0.3s ease-in,opacity 0.3s ease-out; } .ui-scrolltop:hover{ opacity:1; filter:alpha(opacity=100); -moz-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  -webkit-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  -o-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  transition: opacity 0.3s ease-in,opacity 0.3s ease-out; } .ui-scrolltop-hidden{ visibility: hidden; _visibility:visible;_display:none;opacity:0; filter:alpha(opacity=0); -moz-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  -webkit-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  -o-transition: opacity 0.3s ease-in,opacity 0.3s ease-out;  transition: opacity 0.3s ease-in,opacity 0.3s ease-out; }.ui-scrolltop-wb{ background-position:-25px 21px; background-color:#fff; }";
+            return _str_class
         },
         createEl: function () {
-            var D = this,
-				E = D.addCss(),
-				A = '<a id="' + P + '" class="ui-scrolltop ui-scrolltop-hidden ' + (H.type == 2 ? "ui-scrolltop-wb" : "") + '" title="\u8fd4\u56de\u9876\u90e8">\u8fd4\u56de\u9876\u90e8</a>';
-            el = B.create(A);
-            B.addStyleSheet(E);
-            B.append(el, "body");
+            var _backtop = this,
+				_str_class = _backtop.addCss(),
+				_str_dom = '<a id="' + _str_J_ScrollTopBtn + '" class="ui-scrolltop ui-scrolltop-hidden ' + (_options.type == 2 ? "ui-scrolltop-wb" : "") + '" title="返回顶部">返回顶部</a>';
+            el = _dom.create(_str_dom);
+            _dom.addStyleSheet(_str_class);
+            _dom.append(el, "body");
             return el
         },
         bindEvent: function () {
-            var D = this,
-				R = D.scrollEl,
-				A = H,
-				E = A.baseLine;
-            if (!R) {
+            var _backtop = this,
+				_scrollEl = _backtop.scrollEl,
+				_options_t = _options,
+				_baseline = _options_t.baseLine;
+            if (!_scrollEl) {
                 return
             }
-            Q.on(R, "click", function (T) {
-                T.preventDefault();
-                if (I) {
-                    B.addClass(R, F)
+            _event.on(_scrollEl, "click", function (_event) {
+                _event.preventDefault();
+                if (_is_ie6) {
+                    _dom.addClass(_scrollEl, _str_ui_scrolltop_hidden)
                 }
-                A.isAnim ? D.scrollTop(0) : K.scrollTo(0, 0)
+                _options_t.isAnim ? _backtop.scrollTop(0) : _window.scrollTo(0, 0)
             });
-            Q.on(K, "scroll", S);
-            if (L) {
-                Q.on(R, "mouseenter", function () {
-                    C(R, {
+            _event.on(_window, "scroll", S);
+            if (_is_ie) {
+                _event.on(_scrollEl, "mouseenter", function () {
+                    _anim(_scrollEl, {
                         opacity: "1"
                     }, 0.2).run()
                 });
-                Q.on(R, "mouseleave", function () {
-                    C(R, {
+                _event.on(_scrollEl, "mouseleave", function () {
+                    _anim(_scrollEl, {
                         opacity: ".5"
                     }, 0.2).run()
                 })
             }
             function S() {
-                var T = B.hasClass(R, F),
-					W = parseInt(B.scrollTop(N));
-                if (W > E && T) {
-                    if (L) {
-                        C(R, {
+                var _has_hidden_class = _dom.hasClass(_scrollEl, _str_ui_scrolltop_hidden),
+					_scroll_top = parseInt(_dom.scrollTop(_document));
+                if (_scroll_top > _baseline && _has_hidden_class) {
+                    if (_is_ie) {
+                        _anim(_scrollEl, {
                             opacity: "0.5"
                         }, 0.2).run()
                     }
-                    B.removeClass(R, F)
+                    _dom.removeClass(_scrollEl, _str_ui_scrolltop_hidden)
                 } else {
-                    if (W < E && !T) {
-                        if (L) {
-                            C(R, {
+                    if (_scroll_top < _baseline && !_has_hidden_class) {
+                        if (_is_ie) {
+                            _anim(_scrollEl, {
                                 opacity: "0"
                             }, 0.2).run()
                         }
-                        B.addClass(R, F)
+                        _dom.addClass(_scrollEl, _str_ui_scrolltop_hidden)
                     }
                 }
-                if (I) {
-                    var U = B.viewportHeight(N),
-						V = (W + U) - A.bottom - B.height(R);
-                    B.css(R, "top", V)
+                if (_is_ie6) {
+                    var _viewportHeight = _dom.viewportHeight(_document),
+						_top = (_scroll_top + _viewportHeight) - _options_t.bottom - _dom.height(_scrollEl);
+                    _dom.css(_scrollEl, "top", _top)
                 }
             }
         },
         init: function (A) {
-            if (J.get("#" + P)) {
+            if (_kissy_J.get("#" + _str_J_ScrollTopBtn)) {
                 return
             }
-            var D = this,
-				E = M("type"),
-				R = M("anim");
-            if (E) {
-                H.type = E
+            var _backtop = this,
+				_type_option = _get_value_from_attribute("type"),
+				_anim_option = _get_value_from_attribute("anim");
+            if (_type_option) {
+                _options.type = _type_option
             }
-            if (R) {
-                H.isAnim = R
+            if (_anim_option) {
+                _options.isAnim = _anim_option
             }
-            D.scrollEl = D.createEl();
-            D.bindEvent()
+            _backtop.scrollEl = _backtop.createEl();
+            _backtop.bindEvent()
         }
     };
-    J.ready(function () {
-        G.init()
+    _kissy_J.ready(function () {
+        _BackTop.init()
     })
 })(window, KISSY, document);
